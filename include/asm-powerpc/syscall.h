02a2bdf031528e0f3a1a33eb03826a4d88b2494f
Philippe Gerum <rpm@xenomai.org>
2005-11-14 10:06:34 +0000

    Remove duplicate XENOMAI_SYSBIND() definition
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/branches/v2.1@136 c6d672ea-8702-0410-b560-f74c916a59fe

--- include/asm-powerpc/syscall.h
+++ include/asm-powerpc/syscall.h
@@ -157,7 +157,6 @@ static inline int __xn_interrupted_p(struct pt_regs *regs) {
 #define XENOMAI_SYSCALL3(op,a1,a2,a3)       XENOMAI_DO_SYSCALL(3,0,op,a1,a2,a3)
 #define XENOMAI_SYSCALL4(op,a1,a2,a3,a4)    XENOMAI_DO_SYSCALL(4,0,op,a1,a2,a3,a4)
 #define XENOMAI_SYSCALL5(op,a1,a2,a3,a4,a5) XENOMAI_DO_SYSCALL(5,0,op,a1,a2,a3,a4,a5)
-#define XENOMAI_SYSBIND(a1,a2,a3)           XENOMAI_DO_SYSCALL(3,0,__xn_sys_bind,a1,a2,a3)
 #define XENOMAI_SYSBIND(a1,a2,a3,a4)        XENOMAI_DO_SYSCALL(4,0,__xn_sys_bind,a1,a2,a3,a4)
 
 #define XENOMAI_SKINCALL0(id,op)                XENOMAI_DO_SYSCALL(0,id,op)
