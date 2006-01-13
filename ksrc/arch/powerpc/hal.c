50ac68aba4c0b8a96c409009cfbc431fdfd43712
Philippe Gerum <rpm@xenomai.org>
2006-01-13 11:09:24 +0000

    Whine if CONFIG_ALTIVEC enabled but no hw found
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/trunk@448 c6d672ea-8702-0410-b560-f74c916a59fe

--- ksrc/arch/powerpc/hal.c
+++ ksrc/arch/powerpc/hal.c
@@ -395,6 +395,18 @@ RTHAL_DECLARE_DOMAIN(rthal_domain_entry);
 int rthal_arch_init (void)
 
 {
+#ifdef CONFIG_ALTIVEC
+#ifdef CONFIG_PPC64
+    if (!(cur_cpu_spec->cpu_features & CPU_FTR_ALTIVEC)) {
+#else /* !CONFIG_PPC64 */
+    if (!(cur_cpu_spec[0]->cpu_features & CPU_FTR_ALTIVEC)) {
+#endif /* CONFIG_PPC64 */
+        printk("Xenomai: ALTIVEC support enabled in kernel but no hardware found.\n"
+               "         Disable CONFIG_ALTIVEC in the kernel configuration.\n");
+	return -ENODEV;
+    }
+#endif /* CONFIG_ALTIVEC */
+
     if (rthal_cpufreq_arg == 0)
 	/* The CPU frequency is expressed as the timebase frequency
 	   for this port. */
