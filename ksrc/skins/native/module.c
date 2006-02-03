cdea605b1425b4fb66b3880dc254778f956f8f7c
Philippe Gerum <rpm@xenomai.org>
2006-02-03 17:33:10 +0000

    Do not override TIMING_PERIOD
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/trunk@519 c6d672ea-8702-0410-b560-f74c916a59fe

--- ksrc/skins/native/module.c
+++ ksrc/skins/native/module.c
@@ -112,8 +112,6 @@ int SKIN_INIT(native)
 {
     int err;
 
-    nktickdef = XN_APERIODIC_TICK;	/* Defaults to aperiodic. */
-
 #if defined(__KERNEL__) && defined(CONFIG_XENO_OPT_PERVASIVE)
     /* The native skin is stacked over the shared Xenomai pod. */
     err = xncore_attach();
