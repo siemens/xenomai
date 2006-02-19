420a12864ee16a1b0ff8d4645ee364b8521e1b88
Philippe Gerum <rpm@xenomai.org>
2006-02-19 18:35:59 +0000

    Fix issues with rt_event_wait and rt_task_set_periodic

--- skins/native/syscall.c
+++ skins/native/syscall.c
@@ -1373,7 +1373,8 @@ static int __rt_event_wait (struct task_struct *curr, struct pt_regs *regs)
     RTIME timeout;
     int mode, err;
 
-    if (!__xn_access_ok(curr,VERIFY_READ,__xn_reg_arg1(regs),sizeof(ph)))
+    if (!__xn_access_ok(curr,VERIFY_READ,__xn_reg_arg1(regs),sizeof(ph)) ||
+	!__xn_access_ok(curr,VERIFY_WRITE,__xn_reg_arg3(regs),sizeof(mask_r)))
 	return -EFAULT;
 
     __xn_copy_from_user(curr,&ph,(void __user *)__xn_reg_arg1(regs),sizeof(ph));
@@ -3507,7 +3508,7 @@ static xnsysent_t __systab[] = {
     [__xeno_task_resume ] = { &__rt_task_resume, __xn_exec_any },
     [__xeno_task_delete ] = { &__rt_task_delete, __xn_exec_conforming },
     [__xeno_task_yield ] = { &__rt_task_yield, __xn_exec_primary },
-    [__xeno_task_set_periodic ] = { &__rt_task_set_periodic, __xn_exec_primary },
+    [__xeno_task_set_periodic ] = { &__rt_task_set_periodic, __xn_exec_conforming },
     [__xeno_task_wait_period ] = { &__rt_task_wait_period, __xn_exec_primary },
     [__xeno_task_set_priority ] = { &__rt_task_set_priority, __xn_exec_any },
     [__xeno_task_sleep ] = { &__rt_task_sleep, __xn_exec_primary },
