9fd6eeb35bfb67e90cb724e595240139b10fa34a
Philippe Gerum <rpm@xenomai.org>
2006-02-27 10:10:28 +0000

    Cosmetic rename

--- ksrc/nucleus/core.c
+++ ksrc/nucleus/core.c
@@ -20,7 +20,7 @@
 #include <nucleus/pod.h>
 #include <nucleus/core.h>
 
-static xnpod_t __xeno_pod;
+static xnpod_t __core_pod;
 
 static int xncore_unload_hook (void)
 
@@ -28,7 +28,7 @@ static int xncore_unload_hook (void)
     /* If no thread is hosted by the Xenomai pod, unload it. We are
        called with interrupts off, nklock locked. */
 
-    if (nkpod == &__xeno_pod && countq(&nkpod->threadq) == 0)
+    if (nkpod == &__core_pod && countq(&nkpod->threadq) == 0)
 	{
 	xncore_umount();
 	return 1;
@@ -42,19 +42,19 @@ int xncore_attach (void)
 {
     if (nkpod)
 	{
-	if (nkpod != &__xeno_pod)
+	if (nkpod != &__core_pod)
 	    return -ENOSYS;
 
-	++__xeno_pod.refcnt;
+	++__core_pod.refcnt;
 
 	return 0;
 	}
 
-    if (xnpod_init(&__xeno_pod,XNCORE_MIN_PRIO,XNCORE_MAX_PRIO,0) != 0)
+    if (xnpod_init(&__core_pod,XNCORE_MIN_PRIO,XNCORE_MAX_PRIO,0) != 0)
 	return -ENOSYS;
 
-    __xeno_pod.svctable.unload = &xncore_unload_hook;
-    __xeno_pod.refcnt = 1;
+    __core_pod.svctable.unload = &xncore_unload_hook;
+    __core_pod.refcnt = 1;
 
     return 0;
 }
@@ -62,7 +62,7 @@ int xncore_attach (void)
 int xncore_detach (void)
 
 {
-    return --__xeno_pod.refcnt;
+    return --__core_pod.refcnt;
 }
 
 int xncore_mount (void)
@@ -74,7 +74,7 @@ int xncore_mount (void)
 int xncore_umount (void)
 
 {
-    if (nkpod != &__xeno_pod)
+    if (nkpod != &__core_pod)
 	return -ENOSYS;
 
     xnpod_shutdown(XNPOD_NORMAL_EXIT);
