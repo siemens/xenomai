a8d78a098fdee863ab828ebe996d21e40091ba4f
Philippe Gerum <rpm@xenomai.org>
2006-03-10 19:32:22 +0000

    Do not resort to vmalloc for sizes <= 128Kb

--- include/asm-powerpc/system.h
+++ include/asm-powerpc/system.h
@@ -119,7 +119,7 @@ extern "C" {
 static inline void *xnarch_sysalloc (u_long bytes)
 
 {
-    if (bytes >= 128*1024)
+    if (bytes > 128*1024)
 	return vmalloc(bytes);
 
     return kmalloc(bytes,GFP_KERNEL);
@@ -128,7 +128,7 @@ static inline void *xnarch_sysalloc (u_long bytes)
 static inline void xnarch_sysfree (void *chunk, u_long bytes)
 
 {
-    if (bytes >= 128*1024)
+    if (bytes > 128*1024)
 	vfree(chunk);
     else
 	kfree(chunk);
