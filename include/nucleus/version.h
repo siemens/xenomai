6345b31e251e9537dc75113ff2281ae0ed4a8260
Philippe Gerum <rpm@xenomai.org>
2006-03-18 17:57:24 +0000

    Update version information

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Champagne"
+#define XENO_VERSION_NAME	"Engines Of Creation"
 
-#define XENO_VERSION_STRING	"2.1"
+#define XENO_VERSION_STRING	"2.2-dev"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
