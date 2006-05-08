5f377eb52897f07632d41e6d6f2af7b0d3eaebf8
Philippe Gerum <rpm@xenomai.org>
2006-05-08 14:01:57 +0000

    Revert spurious change
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/trunk@1033 c6d672ea-8702-0410-b560-f74c916a59fe

--- ksrc/skins/native/demos/satch.c
+++ ksrc/skins/native/demos/satch.c
@@ -1,3 +1,4 @@
+#include <sys/mman.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -88,7 +89,7 @@ void producer (void *cookie)
 int root_thread_init (void)
 
 {
-    rt_timer_start(1000000);	/* 1ms periodic tick. */
+    rt_timer_set_mode(1000000);	/* Forc 1ms periodic tick. */
 
     rt_task_spawn(&consumer_task,
 		  "ConsumerTask",
@@ -120,6 +121,7 @@ void root_thread_exit (void)
 int main (int ac, char *av[])
 
 {
+    mlockall(MCL_CURRENT|MCL_FUTURE);
     root_thread_init();
     pause();
     root_thread_exit();
