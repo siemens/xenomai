8162778b0a35e3fce62ba882462cbbc346e0b189
Philippe Gerum <rpm@xenomai.org>
2006-05-09 15:01:15 +0000

    Move to K&R style

--- src/skins/native/mutex.c
+++ src/skins/native/mutex.c
@@ -21,56 +21,36 @@
 
 extern int __native_muxid;
 
-int rt_mutex_create (RT_MUTEX *mutex,
-		     const char *name)
+int rt_mutex_create(RT_MUTEX *mutex, const char *name)
 {
     return XENOMAI_SKINCALL2(__native_muxid,
-			     __native_mutex_create,
-			     mutex,
-			     name);
+                             __native_mutex_create, mutex, name);
 }
 
-int rt_mutex_bind (RT_MUTEX *mutex,
-		   const char *name,
-		   RTIME timeout)
+int rt_mutex_bind(RT_MUTEX *mutex, const char *name, RTIME timeout)
 {
     return XENOMAI_SKINCALL3(__native_muxid,
-			     __native_mutex_bind,
-			     mutex,
-			     name,
-			     &timeout);
+                             __native_mutex_bind, mutex, name, &timeout);
 }
 
-int rt_mutex_delete (RT_MUTEX *mutex)
-
+int rt_mutex_delete(RT_MUTEX *mutex)
 {
-    return XENOMAI_SKINCALL1(__native_muxid,
-			     __native_mutex_delete,
-			     mutex);
+    return XENOMAI_SKINCALL1(__native_muxid, __native_mutex_delete, mutex);
 }
 
-int rt_mutex_lock (RT_MUTEX *mutex,
-		   RTIME timeout)
+int rt_mutex_lock(RT_MUTEX *mutex, RTIME timeout)
 {
     return XENOMAI_SKINCALL2(__native_muxid,
-			     __native_mutex_lock,
-			     mutex,
-			     &timeout);
+                             __native_mutex_lock, mutex, &timeout);
 }
 
-int rt_mutex_unlock (RT_MUTEX *mutex)
-
+int rt_mutex_unlock(RT_MUTEX *mutex)
 {
-    return XENOMAI_SKINCALL1(__native_muxid,
-			     __native_mutex_unlock,
-			     mutex);
+    return XENOMAI_SKINCALL1(__native_muxid, __native_mutex_unlock, mutex);
 }
 
-int rt_mutex_inquire (RT_MUTEX *mutex,
-		      RT_MUTEX_INFO *info)
+int rt_mutex_inquire(RT_MUTEX *mutex, RT_MUTEX_INFO *info)
 {
     return XENOMAI_SKINCALL2(__native_muxid,
-			     __native_mutex_inquire,
-			     mutex,
-			     info);
+                             __native_mutex_inquire, mutex, info);
 }
--- src/skins/native/queue.c
+++ src/skins/native/queue.c
@@ -27,95 +27,77 @@
 
 extern int __native_muxid;
 
-static int __map_queue_memory (RT_QUEUE *q, RT_QUEUE_PLACEHOLDER *php)
-
+static int __map_queue_memory(RT_QUEUE *q, RT_QUEUE_PLACEHOLDER * php)
 {
     int err, heapfd;
 
     /* Open the heap device to share the message pool memory with the
        in-kernel skin and bound clients. */
-    heapfd = open(XNHEAP_DEV_NAME,O_RDWR);
+    heapfd = open(XNHEAP_DEV_NAME, O_RDWR);
 
     if (heapfd < 0)
-	return -ENOENT;
+        return -ENOENT;
 
     /* Bind this file instance to the shared heap. */
-    err = ioctl(heapfd,0,php->opaque2);
+    err = ioctl(heapfd, 0, php->opaque2);
 
     if (err)
-	goto close_and_exit;
+        goto close_and_exit;
 
     /* Map the heap memory into our address space. */
-    php->mapbase = (caddr_t)mmap(NULL,
-				 php->mapsize,
-				 PROT_READ|PROT_WRITE,
-				 MAP_SHARED,
-				 heapfd,
-				 0L);
+    php->mapbase = (caddr_t) mmap(NULL,
+                                  php->mapsize,
+                                  PROT_READ | PROT_WRITE,
+                                  MAP_SHARED, heapfd, 0L);
 
     if (php->mapbase != MAP_FAILED)
-	/* Copy back a complete placeholder only if all is ok. */
-	*q = *php;
+        /* Copy back a complete placeholder only if all is ok. */
+        *q = *php;
     else
-	err = -ENOMEM;
+        err = -ENOMEM;
 
- close_and_exit:
+  close_and_exit:
 
     close(heapfd);
 
     return err;
 }
 
-int rt_queue_create (RT_QUEUE *q,
-		     const char *name,
-		     size_t poolsize,
-		     size_t qlimit,
-		     int mode)
+int rt_queue_create(RT_QUEUE *q,
+                    const char *name, size_t poolsize, size_t qlimit, int mode)
 {
     RT_QUEUE_PLACEHOLDER ph;
     int err;
 
     err = XENOMAI_SKINCALL5(__native_muxid,
-			    __native_queue_create,
-			    &ph,
-			    name,
-			    poolsize,
-			    qlimit,
-			    mode|Q_SHARED);
+                            __native_queue_create,
+                            &ph, name, poolsize, qlimit, mode | Q_SHARED);
     if (err)
-	return err;
+        return err;
 
-    err = __map_queue_memory(q,&ph);
+    err = __map_queue_memory(q, &ph);
 
     if (err)
-	/* If the mapping fails, make sure we don't leave a dandling
-	   queue in kernel space -- remove it. */
-	XENOMAI_SKINCALL1(__native_muxid,
-			  __native_queue_delete,
-			  &ph);
+        /* If the mapping fails, make sure we don't leave a dandling
+           queue in kernel space -- remove it. */
+        XENOMAI_SKINCALL1(__native_muxid, __native_queue_delete, &ph);
     return err;
 }
 
-int rt_queue_bind (RT_QUEUE *q,
-		   const char *name,
-		   RTIME timeout)
+int rt_queue_bind(RT_QUEUE *q, const char *name, RTIME timeout)
 {
     RT_QUEUE_PLACEHOLDER ph;
     int err;
 
     err = XENOMAI_SKINCALL3(__native_muxid,
-			    __native_queue_bind,
-			    &ph,
-			    name,
-			    &timeout);
+                            __native_queue_bind, &ph, name, &timeout);
 
-    return err ?: __map_queue_memory(q,&ph);
+    return err ? : __map_queue_memory(q, &ph);
 }
 
-int rt_queue_unbind (RT_QUEUE *q)
-
+int rt_queue_unbind(RT_QUEUE *q)
 {
-    int err = munmap(q->mapbase,q->mapsize);
+    int err = munmap(q->mapbase, q->mapsize);
 
     q->opaque = XN_NO_HANDLE;
     q->mapbase = NULL;
@@ -124,17 +106,14 @@ int rt_queue_unbind (RT_QUEUE *q)
     return err;
 }
 
-int rt_queue_delete (RT_QUEUE *q)
-
+int rt_queue_delete(RT_QUEUE *q)
 {
     int err;
 
-    err = munmap(q->mapbase,q->mapsize);
+    err = munmap(q->mapbase, q->mapsize);
 
     if (!err)
-	err = XENOMAI_SKINCALL1(__native_muxid,
-				__native_queue_delete,
-				q);
+        err = XENOMAI_SKINCALL1(__native_muxid, __native_queue_delete, q);
 
     /* If the deletion fails, there is likely something fishy about
        this queue descriptor, so we'd better clean it up anyway so
@@ -147,82 +126,44 @@ int rt_queue_delete (RT_QUEUE *q)
     return err;
 }
 
-void *rt_queue_alloc (RT_QUEUE *q,
-		      size_t size)
+void *rt_queue_alloc(RT_QUEUE *q, size_t size)
 {
     void *buf;
 
     return XENOMAI_SKINCALL3(__native_muxid,
-			     __native_queue_alloc,
-			     q,
-			     size,
-			     &buf) ? NULL : buf;
+                             __native_queue_alloc, q, size, &buf) ? NULL : buf;
 }
 
-int rt_queue_free (RT_QUEUE *q,
-		   void *buf)
+int rt_queue_free(RT_QUEUE *q, void *buf)
 {
-    return XENOMAI_SKINCALL2(__native_muxid,
-			     __native_queue_free,
-			     q,
-			     buf);
+    return XENOMAI_SKINCALL2(__native_muxid, __native_queue_free, q, buf);
 }
 
-int rt_queue_send (RT_QUEUE *q,
-		   void *buf,
-		   size_t size,
-		   int mode)
+int rt_queue_send(RT_QUEUE *q, void *buf, size_t size, int mode)
 {
     return XENOMAI_SKINCALL4(__native_muxid,
-			     __native_queue_send,
-			     q,
-			     buf,
-			     size,
-			     mode);
+                             __native_queue_send, q, buf, size, mode);
 }
 
-int rt_queue_write (RT_QUEUE *q,
-		    const void *buf,
-		    size_t size,
-		    int mode)
+int rt_queue_write(RT_QUEUE *q, const void *buf, size_t size, int mode)
 {
     return XENOMAI_SKINCALL4(__native_muxid,
-			     __native_queue_write,
-			     q,
-			     buf,
-			     size,
-			     mode);
+                             __native_queue_write, q, buf, size, mode);
 }
 
-ssize_t rt_queue_receive (RT_QUEUE *q,
-			  void **bufp,
-			  RTIME timeout)
+ssize_t rt_queue_receive(RT_QUEUE *q, void **bufp, RTIME timeout)
 {
     return XENOMAI_SKINCALL3(__native_muxid,
-			     __native_queue_receive,
-			     q,
-			     bufp,
-			     &timeout);
+                             __native_queue_receive, q, bufp, &timeout);
 }
 
-ssize_t rt_queue_read (RT_QUEUE *q,
-		       void *buf,
-		       size_t size,
-		       RTIME timeout)
+ssize_t rt_queue_read(RT_QUEUE *q, void *buf, size_t size, RTIME timeout)
 {
     return XENOMAI_SKINCALL4(__native_muxid,
-			     __native_queue_read,
-			     q,
-			     buf,
-			     size,
-			     &timeout);
+                             __native_queue_read, q, buf, size, &timeout);
 }
 
-int rt_queue_inquire (RT_QUEUE *q,
-		      RT_QUEUE_INFO *info)
+int rt_queue_inquire(RT_QUEUE *q, RT_QUEUE_INFO *info)
 {
-    return XENOMAI_SKINCALL2(__native_muxid,
-			     __native_queue_inquire,
-			     q,
-			     info);
+    return XENOMAI_SKINCALL2(__native_muxid, __native_queue_inquire, q, info);
 }
