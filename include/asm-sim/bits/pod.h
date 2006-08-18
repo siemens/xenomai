86bbd840e8bb868885efcad7c3637d1db54fc98d
Philippe Gerum <rpm@xenomai.org>
2006-08-18 14:43:06 +0000

    Shared timer IRQ support

--- include/asm-sim/bits/pod.h
+++ include/asm-sim/bits/pod.h
@@ -154,4 +154,6 @@ static inline unsigned long long xnarch_get_sys_time(void)
     return tv.tv_sec * 1000000000ULL + tv.tv_usec * 1000;
 }
 
+#define xnarch_timer_irq_p()  1
+
 #endif /* !_XENO_ASM_SIM_BITS_POD_H */
