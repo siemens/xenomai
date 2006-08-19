5c87a0f360cc09cc17271472ef223026bb86876d
Philippe Gerum <rpm@xenomai.org>
2006-08-19 12:04:00 +0000

    Bump version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Seven String"
+#define XENO_VERSION_NAME	"Chords Of Life"
 
-#define XENO_VERSION_STRING	"2.1.2"
+#define XENO_VERSION_STRING	"2.1.3"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
