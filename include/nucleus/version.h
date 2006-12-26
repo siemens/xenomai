c203a41332d645550a15038c390e408fb3199a8c
Philippe Gerum <rpm@xenomai.org>
2006-12-26 19:27:04 +0000

    Update version info

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Baroque"
+#define XENO_VERSION_NAME	"Bells Of Lal"
 
-#define XENO_VERSION_STRING	"2.3"
+#define XENO_VERSION_STRING	"2.4-dev"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
