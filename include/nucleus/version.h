221ecb5f3d1d259537922aaa3d55e49e87d07765
Philippe Gerum <rpm@xenomai.org>
2007-03-19 16:14:11 +0000

    Update version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Baroque"
+#define XENO_VERSION_NAME	"One Robot's Dream"
 
-#define XENO_VERSION_STRING	"2.3"
+#define XENO_VERSION_STRING	"2.3.1"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
