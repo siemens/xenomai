6957d5cf2f1dda8b350ad4e037b75c1e04759745
Philippe Gerum <rpm@xenomai.org>
2007-03-20 09:04:34 +0000

    Update version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Redshift Riders"
+#define XENO_VERSION_NAME	"Starry Night"
 
-#define XENO_VERSION_STRING	"2.2.5"
+#define XENO_VERSION_STRING	"2.2.6"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
