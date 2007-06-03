c4b3088a32f6f327093baf065ccf2a6b13fa1a56
Philippe Gerum <rpm@xenomai.org>
2007-06-03 18:07:16 +0000

    Reduce default calibration values
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/trunk@2529 c6d672ea-8702-0410-b560-f74c916a59fe

--- include/asm-i386/calibration.h
+++ include/asm-i386/calibration.h
@@ -36,16 +36,16 @@ static inline unsigned long xnarch_get_sched_latency (void)
 #else
 
 #ifdef CONFIG_X86_LOCAL_APIC
-#define __sched_latency 2700
+#define __sched_latency 1500
 #else /* !CONFIG_X86_LOCAL_APIC */
 
 /* Use the bogomips formula to identify low-end x86 boards when using
    the 8254 PIT. The following is still grossly experimental and needs
    work (i.e. more specific cases), but the approach is definitely
    saner than previous attempts to guess such value dynamically. */
-#define __sched_latency (__bogomips < 250 ? 23200 : \
-                         __bogomips < 2500 ? 6200 : \
-			 6700)
+#define __sched_latency (__bogomips < 250 ? 20000 : \
+                         __bogomips < 2500 ? 5000 : \
+			 5200)
 
 #endif /* CONFIG_X86_LOCAL_APIC */
 #endif /* CONFIG_XENO_OPT_TIMING_SCHEDLAT */
