250ebc562257159c43d170701f262f60440026af
Philippe Gerum <rpm@xenomai.org>
2007-06-08 23:36:38 +0000

    Cosmetic change

--- ksrc/nucleus/module.c
+++ ksrc/nucleus/module.c
@@ -373,14 +373,14 @@ static int stat_seq_open(struct inode *inode, struct file *file)
 
 	xnlock_put_irqrestore(&nklock, s);
 
-	/* The order is important here: first xnintr_list_rev then xnintr_count.
-	 * On the other hand, xnintr_attach/detach() update xnintr_count first
-	 * and then xnintr_list_rev.
-	 * This should guarantee that we can't get an up-to-date xnintr_list_rev
-	 * and old xnintr_count here. The other way around is not a problem as
-	 * xnintr_query() will notice this fact later.
-	 * Should xnintr_list_rev change later, xnintr_query() will trigger
-	 * an appropriate error below. */
+	/* The order is important here: first xnintr_list_rev then
+	 * xnintr_count.  On the other hand, xnintr_attach/detach()
+	 * update xnintr_count first and then xnintr_list_rev.  This
+	 * should guarantee that we can't get an up-to-date
+	 * xnintr_list_rev and old xnintr_count here. The other way
+	 * around is not a problem as xnintr_query() will notice this
+	 * fact later.  Should xnintr_list_rev change later,
+	 * xnintr_query() will trigger an appropriate error below. */
 
 	intr_rev = xnintr_list_rev;
 	xnarch_memory_barrier();
