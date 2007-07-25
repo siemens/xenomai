d3d17553cfa60224e5bad4279d54a42e3c3059d4
Philippe Gerum <rpm@xenomai.org>
2007-07-25 07:49:29 +0000

    Update version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"The Forgotten"
+#define XENO_VERSION_NAME	"Summer Song"
 
-#define XENO_VERSION_STRING	"2.3.2"
+#define XENO_VERSION_STRING	"2.3.3"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
