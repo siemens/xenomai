cae1f7aa0a651ee4b6067d875ea88080f173a963
Philippe Gerum <rpm@xenomai.org>
2007-07-26 13:26:23 +0000

    Make cleaned up threads fully enter the abort state

--- ksrc/skins/posix/thread.c
+++ ksrc/skins/posix/thread.c
@@ -716,9 +716,10 @@ void pse51_threadq_cleanup(pse51_kqueues_t *q)
 	while ((holder = getheadq(&q->threadq)) != NULL) {
 		pthread_t thread = link2pthread(holder);
 
-		/* Raise the abort condition (see xnpod_abort_thread()). */
-		xnthread_set_state(&thread->threadbase, XNDORMANT);
-
+		/* Enter the abort state (see xnpod_abort_thread()). */
+		if (!xnpod_current_p(&thread->threadbase))
+			xnpod_suspend_thread(&thread->threadbase, XNDORMANT,
+					     XN_INFINITE, XN_RELATIVE, NULL);
 		if (pse51_obj_active
 		    (thread, PSE51_THREAD_MAGIC, struct pse51_thread)) {
 			/* Remaining running thread. */
