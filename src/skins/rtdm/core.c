172caa9876422772e2edd9af559424de570825f9
Jan Kiszka <jan.kiszka@web.de>
2007-08-04 12:22:34 +0000

    Reindent
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/trunk@2890 c6d672ea-8702-0410-b560-f74c916a59fe

--- src/skins/rtdm/core.c
+++ src/skins/rtdm/core.c
@@ -23,107 +23,83 @@
 #include <rtdm/rtdm.h>
 #include <rtdm/syscall.h>
 
-
 extern int __rtdm_muxid;
 
 int rt_dev_open(const char *path, int oflag, ...)
 {
-    if (__rtdm_muxid < 0)
-        return -ENODEV;
+	if (__rtdm_muxid < 0)
+		return -ENODEV;
 
-    return XENOMAI_SKINCALL2( __rtdm_muxid,
-                              __rtdm_open,
-                              path,
-                              oflag);
+	return XENOMAI_SKINCALL2(__rtdm_muxid, __rtdm_open, path, oflag);
 }
 
 int rt_dev_socket(int protocol_family, int socket_type, int protocol)
 {
-    if (__rtdm_muxid < 0)
-        return -EAFNOSUPPORT;
+	if (__rtdm_muxid < 0)
+		return -EAFNOSUPPORT;
 
-    return XENOMAI_SKINCALL3( __rtdm_muxid,
-                              __rtdm_socket,
-                              protocol_family,
-                              socket_type,
-                              protocol);
+	return XENOMAI_SKINCALL3(__rtdm_muxid,
+				 __rtdm_socket, protocol_family, socket_type,
+				 protocol);
 }
 
 int rt_dev_close(int fd)
 {
-  return XENOMAI_SKINCALL1( __rtdm_muxid,
-                            __rtdm_close,
-                            fd);
+	return XENOMAI_SKINCALL1(__rtdm_muxid, __rtdm_close, fd);
 }
 
 int rt_dev_ioctl(int fd, int request, ...)
 {
-  va_list ap;
-  void *arg;
+	va_list ap;
+	void *arg;
 
-  va_start(ap, request);
-  arg = va_arg(ap, void*);
-  va_end(ap);
+	va_start(ap, request);
+	arg = va_arg(ap, void*);
+	va_end(ap);
 
-  return XENOMAI_SKINCALL3( __rtdm_muxid,
-                            __rtdm_ioctl,
-                            fd,
-                            request,
-                            arg);
+	return XENOMAI_SKINCALL3(__rtdm_muxid,
+				 __rtdm_ioctl, fd, request, arg);
 }
 
 ssize_t rt_dev_read(int fd, void *buf, size_t nbyte)
 {
-  return XENOMAI_SKINCALL3( __rtdm_muxid,
-                            __rtdm_read,
-                            fd,
-                            buf,
-                            nbyte);
+	return XENOMAI_SKINCALL3(__rtdm_muxid,
+				 __rtdm_read, fd, buf, nbyte);
 }
 
 ssize_t rt_dev_write(int fd, const void *buf, size_t nbyte)
 {
-  return XENOMAI_SKINCALL3( __rtdm_muxid,
-                            __rtdm_write,
-                            fd,
-                            buf,
-                            nbyte);
+	return XENOMAI_SKINCALL3(__rtdm_muxid,
+				 __rtdm_write, fd, buf, nbyte);
 }
 
 ssize_t rt_dev_recvmsg(int fd, struct msghdr *msg, int flags)
 {
-  return XENOMAI_SKINCALL3( __rtdm_muxid,
-                            __rtdm_recvmsg,
-                            fd,
-                            msg,
-                            flags);
+	return XENOMAI_SKINCALL3(__rtdm_muxid,
+				 __rtdm_recvmsg, fd, msg, flags);
 }
 
 ssize_t rt_dev_sendmsg(int fd, const struct msghdr *msg, int flags)
 {
-  return XENOMAI_SKINCALL3( __rtdm_muxid,
-                            __rtdm_sendmsg,
-                            fd,
-                            msg,
-                            flags);
+	return XENOMAI_SKINCALL3(__rtdm_muxid,
+				 __rtdm_sendmsg, fd, msg, flags);
 }
 
 ssize_t rt_dev_recvfrom(int fd, void *buf, size_t len, int flags,
                         struct sockaddr *from,
                         socklen_t *fromlen)
 {
-    struct iovec    iov = {buf, len};
-    struct msghdr   msg =
-        {from, (from != NULL) ? *fromlen : 0, &iov, 1, NULL, 0};
-    int             ret;
+	struct iovec iov = { buf, len };
+	struct msghdr msg = {
+		from, (from != NULL) ? *fromlen : 0, &iov, 1, NULL, 0
+	};
+	int ret;
 
-    ret = XENOMAI_SKINCALL3( __rtdm_muxid,
-                             __rtdm_recvmsg,
-                             fd,
-                             &msg,
-                             flags);
+	ret = XENOMAI_SKINCALL3(__rtdm_muxid,
+				__rtdm_recvmsg, fd, &msg, flags);
 
-    if ((ret >= 0) && (from != NULL))
-        *fromlen = msg.msg_namelen;
-    return ret;
+	if (from && ret >= 0)
+		*fromlen = msg.msg_namelen;
+
+	return ret;
 }
