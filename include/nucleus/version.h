1acdabd265fca383c2802869bc02c9a71865b73f
Philippe Gerum <rpm@xenomai.org>
2007-09-16 08:37:32 +0000

    Update version information

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Summer Song"
+#define XENO_VERSION_NAME	"Cool #9"
 
-#define XENO_VERSION_STRING	"2.3.3.1"
+#define XENO_VERSION_STRING	"2.3.4"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
