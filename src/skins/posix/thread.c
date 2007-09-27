d7107f25c8e8c781a40f7d624bfb1f3ef8c25dbc
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2007-09-27 09:16:19 +0000

    Use the correct attribute reference when creating the real pthread
    git-svn-id: svn+ssh://svn.gna.org/svn/xenomai/trunk@3016 c6d672ea-8702-0410-b560-f74c916a59fe

--- src/skins/posix/thread.c
+++ src/skins/posix/thread.c
@@ -170,7 +170,7 @@ int __wrap_pthread_create(pthread_t *tid,
 	iargs.ret = EAGAIN;
 	__real_sem_init(&iargs.sync, 0, 0);
 
-	err = __real_pthread_create(&ltid, &iattr, &__pthread_trampoline, &iargs);
+	err = __real_pthread_create(&ltid, attr, &__pthread_trampoline, &iargs);
 	if (!err)
 		while (__real_sem_wait(&iargs.sync) && errno == EINTR) ;
 	__real_sem_destroy(&iargs.sync);
