f2faa1c6f21bde6028b73df326cd3047cbc5fdcd
Philippe Gerum <rpm@xenomai.org>
2007-12-09 10:26:11 +0000

    Update version information

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Bells Of Lal"
+#define XENO_VERSION_NAME	"Flying In A Blue Dream"
 
-#define XENO_VERSION_STRING	"2.4.0"
+#define XENO_VERSION_STRING	"2.5-devel"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
