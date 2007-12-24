5d8de4694ede0befea26c09f408e47b31d2f0775
Philippe Gerum <rpm@xenomai.org>
2007-12-24 14:42:52 +0000

    Update version information

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Bells Of Lal"
+#define XENO_VERSION_NAME	"Can't Slow Down"
 
-#define XENO_VERSION_STRING	"2.4.0"
+#define XENO_VERSION_STRING	"2.4.1"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
