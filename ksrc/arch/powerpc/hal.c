b96d13b6b61e919a1fe79dc98fbe8d34f5d648fa
Philippe Gerum <rpm@xenomai.org>
2008-02-10 03:07:11 +0000

    Add HAL support leveraging CONFIG_GENERIC_CLOCKEVENTS for powerpc

--- ksrc/arch/powerpc/hal.c
+++ ksrc/arch/powerpc/hal.c
@@ -33,8 +33,6 @@
  *
  *@{*/
 
-#undef DEBUG
-
 #include <linux/version.h>
 #include <linux/slab.h>
 #include <linux/errno.h>
@@ -67,230 +65,379 @@
 #endif
 
 static struct {
-
-    unsigned long flags;
-    int count;
-
+	unsigned long flags;
+	int count;
 } rthal_linux_irq[IPIPE_NR_XIRQS];
 
+enum rthal_ktimer_mode rthal_ktimer_saved_mode;
+
+#define RTHAL_SET_ONESHOT_XENOMAI	1
+#define RTHAL_SET_ONESHOT_LINUX		2
+#define RTHAL_SET_PERIODIC		3
+
+static inline void rthal_disarm_decr(int disarmed)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,24)
+	disarm_decr[rthal_processor_id()] = disarmed;
+#else
+	per_cpu(disarm_decr, rthal_processor_id()) = disarmed;
+#endif
+}
+
+static inline void rthal_setup_oneshot_dec(void)
+{
+#ifdef CONFIG_40x
+	mtspr(SPRN_TCR, mfspr(SPRN_TCR) & ~TCR_ARE);    /* Auto-reload off. */
+#endif /* CONFIG_40x */
+}
+
+static inline void rthal_setup_periodic_dec(void)
+{
+#ifdef CONFIG_40x
+	mtspr(SPRN_TCR, mfspr(SPRN_TCR) | TCR_ARE); /* Auto-reload on. */
+	mtspr(SPRN_PIT, tb_ticks_per_jiffy);
+#else /* !CONFIG_40x */
+	set_dec(tb_ticks_per_jiffy);
+#endif /* CONFIG_40x */
+}
+
 #ifdef CONFIG_SMP
 
 static void rthal_critical_sync(void)
 {
-    switch (rthal_sync_op) {
-        case 1:
-		/* timer_request */
-		disarm_decr[rthal_processor_id()] = 1;
-            break;
-        case 2:
-		/* timer_release */
-		disarm_decr[rthal_processor_id()] = 0;
-		set_dec(tb_ticks_per_jiffy);
-
-            break;
-        case 3:
-		/* cancel action */
-		disarm_decr[rthal_processor_id()] = 0;
+	switch (rthal_sync_op) {
+	case RTHAL_SET_ONESHOT_XENOMAI:
+		rthal_setup_oneshot_dec();
+		rthal_disarm_decr(1);
 		break;
-    }
+
+	case RTHAL_SET_ONESHOT_LINUX:
+		rthal_setup_oneshot_dec();
+		rthal_disarm_decr(0);
+		/* We need to keep the timing cycle alive for the kernel. */
+		rthal_trigger_irq(RTHAL_TIMER_IRQ);
+		break;
+
+	case RTHAL_SET_PERIODIC:
+		rthal_setup_periodic_dec();
+		rthal_disarm_decr(0);
+		break;
+	}
 }
+
 #else /* CONFIG_SMP */
+
 static void rthal_critical_sync(void)
 {
 }
+
 #endif /* !CONFIG_SMP */
 
-#ifdef CONFIG_SMP
-static void rthal_smp_relay_tick(unsigned irq, void *cookie)
+static void rthal_timer_set_oneshot(int rt_mode)
 {
-    rthal_irq_host_pend(RTHAL_TIMER_IRQ);
+	unsigned long flags;
+
+	flags = rthal_critical_enter(rthal_critical_sync);
+	if (rt_mode) {
+		rthal_sync_op = RTHAL_SET_ONESHOT_XENOMAI;
+		rthal_setup_oneshot_dec();
+		rthal_disarm_decr(1);
+	} else {
+		rthal_sync_op = RTHAL_SET_ONESHOT_LINUX;
+		rthal_setup_oneshot_dec();
+		rthal_disarm_decr(0);
+		/* We need to keep the timing cycle alive for the kernel. */
+		rthal_trigger_irq(RTHAL_TIMER_IRQ);
+	}
+	rthal_critical_exit(flags);
 }
-#endif /* CONFIG_SMP */
 
-int rthal_timer_request(void (*handler)(void), int cpu)
+static void rthal_timer_set_periodic(void)
 {
-    unsigned long flags;
-    int err = 0;
+	unsigned long flags;
 
-    if (cpu > 0)
-	    goto done;
+	flags = rthal_critical_enter(&rthal_critical_sync);
+	rthal_sync_op = RTHAL_SET_PERIODIC;
+	rthal_setup_periodic_dec();
+	rthal_disarm_decr(0);
+	rthal_critical_exit(flags);
+}
 
-    flags = rthal_critical_enter(&rthal_critical_sync);
+static int cpu_timers_requested;
 
-    rthal_sync_op = 1;
+#ifdef CONFIG_GENERIC_CLOCKEVENTS
 
-#ifdef CONFIG_40x
-    mtspr(SPRN_TCR, mfspr(SPRN_TCR) & ~TCR_ARE);    /* Auto-reload off. */
-#endif /* CONFIG_40x */
-    rthal_timer_program_shot(tb_ticks_per_jiffy);
+int rthal_timer_request(
+	void (*tick_handler)(void),
+	void (*mode_emul)(enum clock_event_mode mode,
+			  struct clock_event_device *cdev),
+	int (*tick_emul)(unsigned long delay,
+			 struct clock_event_device *cdev),
+	int cpu)
+{
+	int tickval, err, res;
+	unsigned long tmfreq;
 
-    if (err)
-        goto out;
+	res = ipipe_request_tickdev("decrementer", mode_emul, tick_emul, cpu,
+				    &tmfreq);
+	/*
+	 * Ignore the returned timer freq; the timebase freq is more
+	 * accurate (CPU_FREQ == timebase freq for this port).
+	 */
+	switch (res) {
+	case CLOCK_EVT_MODE_PERIODIC:
+		/* oneshot tick emulation callback won't be used, ask
+		 * the caller to start an internal timer for emulating
+		 * a periodic tick. */
+		tickval = 1000000000UL / HZ;
+		break;
 
-    rthal_irq_release(RTHAL_TIMER_IRQ);
-    if ((err = rthal_irq_request(RTHAL_TIMER_IRQ,
-                                 (rthal_irq_handler_t) handler,
-                                 NULL, NULL)) < 0) {
-        goto out;
-    }
+	case CLOCK_EVT_MODE_ONESHOT:
+		/* oneshot tick emulation */
+		tickval = 1;
+		break;
+
+	case CLOCK_EVT_MODE_UNUSED:
+		/* we don't need to emulate the tick at all. */
+		tickval = 0;
+		break;
+
+	case CLOCK_EVT_MODE_SHUTDOWN:
+		return -ENODEV;
+
+	default:
+		return res;
+	}
+	rthal_ktimer_saved_mode = res;
+
+	/*
+	 * The rest of the initialization should only be performed
+	 * once by a single CPU.
+	 */
+	if (cpu_timers_requested++ > 0)
+		goto out;
+
+	err = rthal_irq_request(RTHAL_TIMER_IRQ,
+				(rthal_irq_handler_t) tick_handler,
+				NULL, NULL);
+	if (err)
+		return err;
+
 #ifdef CONFIG_SMP
-    rthal_irq_release(RTHAL_TIMER_IPI);
-    if ((err = rthal_irq_request(RTHAL_TIMER_IPI,
-                                 (rthal_irq_handler_t) handler,
-                                 NULL, NULL)) < 0) {
-        rthal_irq_release(RTHAL_TIMER_IRQ);
-        goto out;
-    }
-    rthal_irq_release(RTHAL_HOST_TIMER_IPI);
-    if ((err = rthal_irq_request(RTHAL_HOST_TIMER_IPI,
-                                 &rthal_smp_relay_tick, NULL, NULL)) < 0) {
-        rthal_irq_release(RTHAL_TIMER_IRQ);
-        goto out;
-    }
-#endif /* CONFIG_SMP */
+	err = rthal_irq_request(RTHAL_TIMER_IPI,
+				(rthal_irq_handler_t) tick_handler,
+				NULL, NULL);
+	if (err)
+		return err;
+#endif
 
-    disarm_decr[rthal_processor_id()] = 1;
+	rthal_timer_set_oneshot(1);
 
 out:
-    if (err) {
-        rthal_sync_op = 3;
-        __ipipe_decr_ticks = tb_ticks_per_jiffy;
-        disarm_decr[rthal_processor_id()] = 0;
-    }
-    rthal_critical_exit(flags);
-
-done:
-
-    return err;
+	return tickval;
 }
 
 void rthal_timer_release(int cpu)
 {
-    unsigned long flags;
+	ipipe_release_tickdev(cpu);
 
-    if (cpu > 0)
-	    return;
-
-    flags = rthal_critical_enter(&rthal_critical_sync);
-
-    rthal_sync_op = 2;
-
-#ifdef CONFIG_40x
-    mtspr(SPRN_TCR, mfspr(SPRN_TCR) | TCR_ARE); /* Auto-reload on. */
-    mtspr(SPRN_PIT, tb_ticks_per_jiffy);
-#else /* !CONFIG_40x */
-    set_dec(tb_ticks_per_jiffy);
-#endif /* CONFIG_40x */
+	if (--cpu_timers_requested > 0)
+		return;
 
 #ifdef CONFIG_SMP
-    rthal_irq_release(RTHAL_HOST_TIMER_IPI);
-    rthal_irq_release(RTHAL_TIMER_IPI);
+	rthal_irq_release(RTHAL_TIMER_IPI);
 #endif /* CONFIG_SMP */
-    rthal_irq_release(RTHAL_TIMER_IRQ);
+	rthal_irq_release(RTHAL_TIMER_IRQ);
 
-    disarm_decr[rthal_processor_id()] = 0;
-
-    rthal_critical_exit(flags);
+	if (rthal_ktimer_saved_mode == KTIMER_MODE_PERIODIC)
+		rthal_timer_set_periodic();
+	else if (rthal_ktimer_saved_mode == KTIMER_MODE_ONESHOT)
+		rthal_timer_set_oneshot(0);
 }
+
+void rthal_timer_notify_switch(enum clock_event_mode mode,
+			       struct clock_event_device *cdev)
+{
+	if (rthal_processor_id() > 0)
+		/*
+		 * We assume all CPUs switch the same way, so we only
+		 * track mode switches from the boot CPU.
+		 */
+		return;
+
+	rthal_ktimer_saved_mode = mode;
+}
+
+EXPORT_SYMBOL(rthal_timer_notify_switch);
+
+#else /* !CONFIG_GENERIC_CLOCKEVENTS */
+
+static void rthal_smp_relay_tick(unsigned irq, void *cookie)
+{
+	rthal_irq_host_pend(RTHAL_TIMER_IRQ);
+}
+
+int rthal_timer_request(void (*handler)(void), int cpu)
+{
+	int err;
+	/*
+	 * The rest of the initialization should only be performed
+	 * once by a single CPU.
+	 */
+	if (cpu_timers_requested++ > 0)
+		return 0;
+
+	rthal_ktimer_saved_mode = KTIMER_MODE_PERIODIC;
+
+	if (rthal_timerfreq_arg == 0)
+		rthal_tunables.timer_freq = rthal_cpufreq_arg;
+
+	err = rthal_irq_request(RTHAL_TIMER_IRQ,
+				(rthal_irq_handler_t) handler,
+				NULL, NULL);
+	if (err)
+		return err;
+
+#ifdef CONFIG_SMP
+	err = rthal_irq_request(RTHAL_TIMER_IPI,
+				(rthal_irq_handler_t) handler,
+				NULL, NULL);
+	if (err)
+		return err;
+	err = rthal_irq_request(RTHAL_HOST_TIMER_IPI,
+				rthal_smp_relay_tick,
+				NULL, NULL);
+	if (err)
+		return err;
+#endif
+
+	rthal_timer_set_oneshot(1);
+
+	return 0;
+}
+
+void rthal_timer_release(int cpu)
+{
+	unsigned long flags;
+
+	if (cpu > 0)
+		return;
+
+	flags = rthal_critical_enter(&rthal_critical_sync);
+
+	rthal_sync_op = RTHAL_SET_PERIODIC;
+	rthal_setup_periodic_dec();
+	rthal_disarm_decr(0);
+
+#ifdef CONFIG_SMP
+	rthal_irq_release(RTHAL_HOST_TIMER_IPI);
+	rthal_irq_release(RTHAL_TIMER_IPI);
+#endif /* CONFIG_SMP */
+	rthal_irq_release(RTHAL_TIMER_IRQ);
+
+	rthal_critical_exit(flags);
+}
+
+#endif /* !CONFIG_GENERIC_CLOCKEVENTS */
 
 unsigned long rthal_timer_calibrate(void)
 {
-    return 1000000000 / RTHAL_CPU_FREQ;
+	return 1000000000 / RTHAL_CPU_FREQ;
 }
 
 int rthal_irq_host_request(unsigned irq,
                            rthal_irq_host_handler_t handler,
                            char *name, void *dev_id)
 {
-    unsigned long flags;
+	unsigned long flags;
 
-    if (irq >= IPIPE_NR_XIRQS || !handler)
-        return -EINVAL;
+	if (irq >= IPIPE_NR_XIRQS || !handler)
+		return -EINVAL;
 
-    spin_lock_irqsave(&rthal_irq_descp(irq)->lock, flags);
+	spin_lock_irqsave(&rthal_irq_descp(irq)->lock, flags);
 
-    if (rthal_linux_irq[irq].count++ == 0 && rthal_irq_descp(irq)->action) {
-        rthal_linux_irq[irq].flags = rthal_irq_descp(irq)->action->flags;
-        rthal_irq_descp(irq)->action->flags |= IRQF_SHARED;
-    }
+	if (rthal_linux_irq[irq].count++ == 0 && rthal_irq_descp(irq)->action) {
+		rthal_linux_irq[irq].flags = rthal_irq_descp(irq)->action->flags;
+		rthal_irq_descp(irq)->action->flags |= IRQF_SHARED;
+	}
 
-    spin_unlock_irqrestore(&rthal_irq_descp(irq)->lock, flags);
+	spin_unlock_irqrestore(&rthal_irq_descp(irq)->lock, flags);
 
-    return request_irq(irq, handler, IRQF_SHARED, name, dev_id);
+	return request_irq(irq, handler, IRQF_SHARED, name, dev_id);
 }
 
 int rthal_irq_host_release(unsigned irq, void *dev_id)
 {
-    unsigned long flags;
+	unsigned long flags;
 
-    if (irq >= IPIPE_NR_XIRQS || rthal_linux_irq[irq].count == 0)
-        return -EINVAL;
+	if (irq >= IPIPE_NR_XIRQS || rthal_linux_irq[irq].count == 0)
+		return -EINVAL;
 
-    free_irq(irq, dev_id);
+	free_irq(irq, dev_id);
 
-    spin_lock_irqsave(&rthal_irq_descp(irq)->lock, flags);
+	spin_lock_irqsave(&rthal_irq_descp(irq)->lock, flags);
 
-    if (--rthal_linux_irq[irq].count == 0 && rthal_irq_descp(irq)->action)
-        rthal_irq_descp(irq)->action->flags = rthal_linux_irq[irq].flags;
+	if (--rthal_linux_irq[irq].count == 0 && rthal_irq_descp(irq)->action)
+		rthal_irq_descp(irq)->action->flags = rthal_linux_irq[irq].flags;
 
-    spin_unlock_irqrestore(&rthal_irq_descp(irq)->lock, flags);
+	spin_unlock_irqrestore(&rthal_irq_descp(irq)->lock, flags);
 
-    return 0;
+	return 0;
 }
 
 int rthal_irq_enable(unsigned irq)
 {
-    if (irq >= NR_IRQS)
-        return -EINVAL;
+	if (irq >= NR_IRQS)
+		return -EINVAL;
 
-    rthal_irq_desc_status(irq) &= ~IRQ_DISABLED;
+	rthal_irq_desc_status(irq) &= ~IRQ_DISABLED;
 
-    return rthal_irq_chip_enable(irq);
+	return rthal_irq_chip_enable(irq);
 }
 
 int rthal_irq_disable(unsigned irq)
 {
 
-    if (irq >= NR_IRQS)
-        return -EINVAL;
+	if (irq >= NR_IRQS)
+		return -EINVAL;
 
-    rthal_irq_desc_status(irq) |= IRQ_DISABLED;
+	rthal_irq_desc_status(irq) |= IRQ_DISABLED;
 
-    return rthal_irq_chip_disable(irq);
+	return rthal_irq_chip_disable(irq);
 }
 
 int rthal_irq_end(unsigned irq)
 {
-    if (irq >= NR_IRQS)
-        return -EINVAL;
+	if (irq >= NR_IRQS)
+		return -EINVAL;
 
-    return rthal_irq_chip_end(irq);
+	return rthal_irq_chip_end(irq);
 }
 
 static inline int do_exception_event(unsigned event, unsigned domid, void *data)
 {
-    if (domid == RTHAL_DOMAIN_ID) {
-	    rthal_realtime_faults[rthal_processor_id()][event]++;
+	if (domid == RTHAL_DOMAIN_ID) {
+		rthal_realtime_faults[rthal_processor_id()][event]++;
 
-	    if (rthal_trap_handler != NULL &&
-		rthal_trap_handler(event, domid, data) != 0)
-		    return RTHAL_EVENT_STOP;
-    }
+		if (rthal_trap_handler != NULL &&
+		    rthal_trap_handler(event, domid, data) != 0)
+			return RTHAL_EVENT_STOP;
+	}
 
-    return RTHAL_EVENT_PROPAGATE;
+	return RTHAL_EVENT_PROPAGATE;
 }
 
 RTHAL_DECLARE_EVENT(exception_event);
 
 static inline void do_rthal_domain_entry(void)
 {
-    unsigned trapnr;
+	unsigned trapnr;
 
-    /* Trap all faults. */
-    for (trapnr = 0; trapnr < RTHAL_NR_FAULTS; trapnr++)
-        rthal_catch_exception(trapnr, &exception_event);
+	/* Trap all faults. */
+	for (trapnr = 0; trapnr < RTHAL_NR_FAULTS; trapnr++)
+		rthal_catch_exception(trapnr, &exception_event);
 
-    printk(KERN_INFO "Xenomai: hal/powerpc started.\n");
+	printk(KERN_INFO "Xenomai: hal/powerpc started.\n");
 }
 
 RTHAL_DECLARE_DOMAIN(rthal_domain_entry);
@@ -298,29 +445,26 @@ RTHAL_DECLARE_DOMAIN(rthal_domain_entry);
 int rthal_arch_init(void)
 {
 #ifdef CONFIG_ALTIVEC
-    if (!cpu_has_feature(CPU_FTR_ALTIVEC)) {
-        printk
-            ("Xenomai: ALTIVEC support enabled in kernel but no hardware found.\n"
-             "         Disable CONFIG_ALTIVEC in the kernel configuration.\n");
-        return -ENODEV;
-    }
+	if (!cpu_has_feature(CPU_FTR_ALTIVEC)) {
+		printk
+			("Xenomai: ALTIVEC support enabled in kernel but no hardware found.\n"
+			 "         Disable CONFIG_ALTIVEC in the kernel configuration.\n");
+		return -ENODEV;
+	}
 #endif /* CONFIG_ALTIVEC */
 
-    if (rthal_cpufreq_arg == 0)
-        /* The CPU frequency is expressed as the timebase frequency
-           for this port. */
-        rthal_cpufreq_arg = (unsigned long)rthal_get_cpufreq();
+	if (rthal_cpufreq_arg == 0)
+		/* The CPU frequency is expressed as the timebase frequency
+		   for this port. */
+		rthal_cpufreq_arg = (unsigned long)rthal_get_cpufreq();
 
-    if (rthal_timerfreq_arg == 0)
-        rthal_timerfreq_arg = rthal_cpufreq_arg;
-
-    return 0;
+	return 0;
 }
 
 void rthal_arch_cleanup(void)
 {
-    /* Nothing to cleanup so far. */
-    printk(KERN_INFO "Xenomai: hal/powerpc stopped.\n");
+	/* Nothing to cleanup so far. */
+	printk(KERN_INFO "Xenomai: hal/powerpc stopped.\n");
 }
 
 /*@}*/
