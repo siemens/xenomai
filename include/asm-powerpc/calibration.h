eb714e2e1055d26417b205b7998d1bf3d8785197
Philippe Gerum <rpm@xenomai.org>
2008-02-10 14:04:06 +0000

    Reduce pre-calibrated latency for the PA-Semi

--- include/asm-powerpc/calibration.h
+++ include/asm-powerpc/calibration.h
@@ -36,9 +36,9 @@ static inline unsigned long xnarch_get_sched_latency(void)
 
 #if defined(CONFIG_PPC_PASEMI)
 #ifdef CONFIG_SMP
-#define __sched_latency 9000
+#define __sched_latency 8000
 #else
-#define __sched_latency 7000
+#define __sched_latency 6000
 #endif
 #elif defined(CONFIG_WALNUT)
 #define __sched_latency 11000
