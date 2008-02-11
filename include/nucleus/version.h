b438b84414265b5c96b84b3da8abfe990267b291
Philippe Gerum <rpm@xenomai.org>
2008-02-11 11:02:01 +0000

    Update version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Can't Slow Down"
+#define XENO_VERSION_NAME	"One Big Rush"
 
-#define XENO_VERSION_STRING	"2.4.1"
+#define XENO_VERSION_STRING	"2.4.2"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
