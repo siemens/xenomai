34d0c18c48bd2234c8cc16cd4710f7c1db790ca7
Philippe Gerum <rpm@xenomai.org>
2008-03-17 09:34:07 +0000

    Add xnarch_remap_kmem_page_range() wrapper

--- include/asm-generic/system.h
+++ include/asm-generic/system.h
@@ -414,6 +414,15 @@ static inline int xnarch_remap_io_page_range(struct vm_area_struct *vma,
     return wrap_remap_io_page_range(vma,from,to,size,prot);
 }
 
+static inline int xnarch_remap_kmem_page_range(struct vm_area_struct *vma,
+					       unsigned long from,
+					       unsigned long to,
+					       unsigned long size,
+					       pgprot_t prot)
+{
+    return wrap_remap_kmem_page_range(vma,from,to,size,prot);
+}
+
 #ifdef __cplusplus
 }
 #endif
