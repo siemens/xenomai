4b1ef5228b60c2a61f551c876665b865f076093f
Philippe Gerum <rpm@xenomai.org>
2008-05-22 10:28:27 +0000

    Update version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Back to Shalla-Bal"
+#define XENO_VERSION_NAME	"Made Of Tears"
 
-#define XENO_VERSION_STRING	"2.4.3"
+#define XENO_VERSION_STRING	"2.4.4"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
