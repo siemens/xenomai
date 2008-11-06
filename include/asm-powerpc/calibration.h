414c56a33ada0d0311e29be4523d658256602b20
Philippe Gerum <rpm@xenomai.org>
2008-11-06 21:45:21 +0000

    Nullify intrinsic latency value for PASEMI to account for significant jitter reduction

--- include/asm-powerpc/calibration.h
+++ include/asm-powerpc/calibration.h
@@ -35,11 +35,7 @@ static inline unsigned long xnarch_get_sched_latency(void)
 
 
 #if defined(CONFIG_PPC_PASEMI)
-#ifdef CONFIG_SMP
-#define __sched_latency 5000
-#else
-#define __sched_latency 3000
-#endif
+#define __sched_latency 0
 #elif defined(CONFIG_WALNUT)
 #define __sched_latency 11000
 #elif defined(CONFIG_BUBINGA)
