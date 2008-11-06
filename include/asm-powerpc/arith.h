5678fcf69aacb48afd666ab04e70551db3fc7309
Philippe Gerum <rpm@xenomai.org>
2008-11-06 21:45:49 +0000

    Provide native nodiv_ullimd support to 64bit platforms

--- include/asm-powerpc/arith.h
+++ include/asm-powerpc/arith.h
@@ -3,6 +3,33 @@
 
 #define XNARCH_WANT_NODIV_MULDIV
 
+#ifdef __powerpc64__
+
+#define rthal_nodiv_ullimd(op, frac, integ) \
+	rthal_powerpc64_nodiv_ullimd((op), (frac), (integ))
+
+static inline __attribute__((__const__)) unsigned long long
+rthal_powerpc64_nodiv_ullimd(const unsigned long long op,
+			     const unsigned long long frac,
+			     const unsigned rhs_integ)
+{
+	unsigned long h, l, m;
+
+	__asm__("mulhdu   %0, %3, %4\n\t"			\
+		"mulld    %1, %3, %4\n\t"			\
+		"rlwinm   %2, %1, 0, 0, 0\n\t"			\
+		"sldi     %2, %2, 1\n\t"			\
+		"addc     %1, %1, %2\n\t"			\
+		"addze    %0, %0\n\t"				\
+		"adde     %0, %0, %5\n\t"			\
+		: "=&r"(h), "=&r"(l), "=&r"(m)			\
+		: "r"(op), "r"(frac), "r"(rhs_integ) : "cc");	\
+	  
+	return h;
+}
+
+#else /* !__powerpc64__ */
+
 #define __rthal_add96and64(l0, l1, l2, s0, s1)		\
 	do {						\
 		__asm__ ("addc %2, %2, %4\n\t"		\
@@ -11,6 +38,8 @@
 			 : "+r"(l0), "+r"(l1), "+r"(l2)	\
 			 : "r"(s0), "r"(s1) : "cc");	\
 	} while (0)
+
+#endif /* !__powerpc64__ */
 
 #include <asm-generic/xenomai/arith.h>
 
