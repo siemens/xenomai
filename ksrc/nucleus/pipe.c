77af4a715664f04512fb0ab79b845b8ce4823000
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2008-11-24 22:45:13 +0000

    Remove pipe from sleepq upon release

--- ksrc/nucleus/pipe.c
+++ ksrc/nucleus/pipe.c
@@ -94,10 +94,21 @@ static inline void xnpipe_enqueue_wait(xnpipe_state_t *state, int mask)
 
 static inline void xnpipe_dequeue_wait(xnpipe_state_t *state, int mask)
 {
-	if (testbits(state->status, mask)) {
-		if (--state->wcount == 0)
+	if (testbits(state->status, mask))
+		if (--state->wcount == 0) {
 			removeq(&xnpipe_sleepq, &state->slink);
-		__clrbits(state->status, mask);
+			__clrbits(state->status, mask);
+		}
+}
+
+static inline void xnpipe_dequeue_all(xnpipe_state_t *state, int mask)
+{
+	if (testbits(state->status, mask)) {
+		if (state->wcount) {
+			state->wcount = 0;
+			removeq(&xnpipe_sleepq, &state->slink);
+			__clrbits(state->status, mask);
+		}
 	}
 }
 
@@ -651,8 +662,8 @@ static int xnpipe_release(struct inode *inode, struct file *file)
 
 	xnlock_get_irqsave(&nklock, s);
 
-	xnpipe_dequeue_wait(state, XNPIPE_USER_WREAD);
-	xnpipe_dequeue_wait(state, XNPIPE_USER_WSYNC);
+	xnpipe_dequeue_all(state, XNPIPE_USER_WREAD);
+	xnpipe_dequeue_all(state, XNPIPE_USER_WSYNC);
 
 	if (testbits(state->status, XNPIPE_KERN_CONN)) {
 		/* If a Xenomai thread is waiting on this object, wake
