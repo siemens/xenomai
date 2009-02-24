e01a46f61c3bc11d6697b1998865e36259b77bcb
Philippe Gerum <rpm@xenomai.org>
2009-02-24 17:42:48 +0000

    Bump version number

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Bamboo"
+#define XENO_VERSION_NAME	"Andalusia"
 
-#define XENO_VERSION_STRING	"2.4.6"
+#define XENO_VERSION_STRING	"2.4.7"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
