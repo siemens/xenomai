47586ce140aff390e7e122ed4e1b9353a2467624
Philippe Gerum <rpm@xenomai.org>
2009-05-13 02:27:17 +0200

    Add calibration default for Emerson's MVME7100 SBC (MPC8641D)

--- include/asm-powerpc/calibration.h
+++ include/asm-powerpc/calibration.h
@@ -64,6 +64,8 @@ static inline unsigned long xnarch_get_sched_latency(void)
 #define __sched_latency 2900
 #elif defined(CONFIG_MPC5121_ADS)
 #define __sched_latency 4000
+#elif defined(CONFIG_MVME7100)
+#define __sched_latency 1500
 /*
  * Check for the most generic configs at the bottom of this list, so
  * that the most specific choices available are picked first.
