d15a76f812e996d73a307f5d7bc5d2d052e47e97
Philippe Gerum <rpm@xenomai.org>
2009-05-28 10:11:45 +0200

    Update release stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Andalusia"
+#define XENO_VERSION_NAME	"Lords Of Karma"
 
-#define XENO_VERSION_STRING	"2.4.7"
+#define XENO_VERSION_STRING	"2.4.8"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
