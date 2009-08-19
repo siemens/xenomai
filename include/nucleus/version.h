17dd7616783ed1446745a68bacf18e19f423e812
Philippe Gerum <rpm@xenomai.org>
2009-08-19 15:53:40 +0200

    build: update version stamp

--- include/nucleus/version.h
+++ include/nucleus/version.h
@@ -26,8 +26,8 @@
 					    CONFIG_XENO_VERSION_MINOR, \
 					    CONFIG_XENO_REVISION_LEVEL)
 
-#define XENO_VERSION_NAME	"Lords Of Karma"
+#define XENO_VERSION_NAME	"Big Bad Moon"
 
-#define XENO_VERSION_STRING	"2.4.8"
+#define XENO_VERSION_STRING	"2.4.9"
 
 #endif /* _XENO_NUCLEUS_VERSION_H */
