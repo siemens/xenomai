ea9279f4bae53185ae03196922171bde4795798e
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2009-09-20 23:09:39 +0200

    build: provide front-end script to link static POSIX apps
    An unfortunate side-effect of using ld's --wrap magic for shadowing
    libpthread symbols by Xenomai's POSIX skin calls, is that we end up
    wrapping libpthread's own symbols as well, in case of static linking.
    
    This patch introduces the wrap-link.sh script, which reshuffles a
    linker invocation, in order to split it in two separate stages. First,
    the wrappable bits are combined into a single relocatable object,
    then, the latter object is linked against libpthread without
    performing any wrapping.
    
    Typically, invoking:
    
    $ wrap-link.sh gcc -o foo foo.o -Wl,@/usr/xenomai/lib/posix.wrappers -L/usr/xenomai/lib -lpthread_rt -lpthread -lrt
    
    will produce:
    
    stage1: gcc -o foo.tmp -Wl,-r -nostdlib tmp foo.o -Wl,@/usr/xenomai/lib/posix.wrappers -L/usr/xenomai/lib
    stage2: gcc -o foo foo.tmp -lpthread_rt -lpthread -lrt
    
    This means that symbol wrapping will only occur during stage1 of the
    linking process, thus preventing spurious wrapping of libpthread
    symbols in stage2.
    
    Makefiles used in building applications which depend on the POSIX skin
    have been updated in order to use that front-end.
    
    NOTE: this commit clears the previous restriction imposed on the
    recent Xenomai/nios2 port regarding POSIX app generation.

---
+++ scripts/wrap-link.sh
@@ -0,0 +1,174 @@
+#! /bin/sh
+# Split link-edit into two stage for linking static applications with
+# Xenomai user-space posix skin.
+
+set -e
+
+usage() {
+    cat <<EOF
+$progname [options] command-line
+
+Split command-line in two parts for linking static applications with
+Xenomai user-space posix skin in two stages.
+
+Options:
+-q be quiet
+-v be verbose (print each command before running it)
+
+Example:
+$progname -v gcc -o foo foo.o -Wl,@/usr/xenomai/lib/posix.wrappers -L/usr/xenomai/lib -lpthread_rt -lpthread -lrt
+will print and run:
++ gcc -o foo.tmp -Wl,-r -nostdlib tmp foo.o -Wl,@/usr/xenomai/lib/posix.wrappers -L/usr/xenomai/lib
++ gcc -o foo foo.tmp -lpthread_rt -lpthread -lrt
++ rm foo.tmp
+EOF
+}
+
+add_linker_flag() {
+    if $next_is_wrapped_symbol; then
+	stage1_args="$stage1_args -Wl,--wrap $@"
+	next_is_wrapped_symbol=false
+    else
+	stage1_args="$stage1_args $@"
+	stage2_args="$stage2_args $@"
+    fi
+}
+
+add_linker_obj() {
+    if $stage2; then
+	stage2_args="$stage2_args $@"
+    else
+	stage1_args="$stage1_args $@"
+    fi
+}
+
+if test -n "$V" && test $V -gt 0; then
+    verbose=:
+else
+    verbose=false
+fi
+progname=$0
+
+if test $# -eq 0; then
+    usage
+    exit 0
+fi
+
+while test $# -gt 0; do
+    arg="$1"
+    shift
+    case "$arg" in
+	"")
+	    usage
+	    exit 0
+	    ;;
+
+	-v) 
+	    verbose=:
+	    ;;
+
+	-q) 
+	    verbose=false
+	    ;;
+
+	-*)
+	    cc="$cc $arg"
+	    ;;
+
+	*gcc*|*g++*)
+	    cc="$cc $arg"
+	    break
+	    ;;
+
+	*ld)
+	    usage
+	    /bin/echo -e "\nlinker must be gcc or g++, not ld"
+	    exit 1
+	    ;;
+
+	*)
+	    cc="$cc $arg"
+	    ;;
+    esac
+done
+
+next_is_wrapped_symbol=false
+stage1_args=""
+stage2_args=""
+stage2=false
+while test $# -gt 0; do
+    arg="$1"
+    shift
+    case "$arg" in
+	*pthread_rt*|-lpthread)
+	    stage2_args="$stage2_args $arg"
+	    stage2=:
+	    ;;
+
+	-Xlinker)
+	    arg="$1"
+	    shift
+	    case "$arg" in
+		--wrap)
+		    next_is_wrapped_symbol=:
+		    ;;
+		@*posix.wrappers)
+		    stage1_args="$stage1_args -Xlinker $arg"
+		    ;;
+
+		*) 
+		    add_linker_flag -Xlinker "$arg"
+		    ;;
+	    esac
+	    ;;
+
+	-Wl,--wrap)
+	    next_is_wrapped_symbol=:
+	    ;;
+
+	-Wl,@*posix.wrappers|-Wl,--wrap,*)
+	    stage1_args="$stage1_args $arg"
+	    ;;
+
+	-Wl,*)
+	    add_linker_flag "$arg"
+	    ;;
+
+	-o)
+	    output="$1"
+	    shift
+	    ;;
+
+	-o*)
+	    output=`expr $arg : '-o\(.*\)'`
+	    ;;
+	
+	-l) 
+	    arg="$1"
+	    shift
+	    add_linker_obj -l $arg
+	    ;;
+
+	-l*) #a library
+	    add_linker_obj $arg
+	    ;;
+
+	*.so)
+	    stage2_args="$stage2_args $arg"
+	    ;;
+
+	*) 
+	    if test -e "$arg"; then
+		add_linker_obj $arg
+	    else
+		stage1_args="$stage1_args $arg"
+		stage2_args="$stage2_args $arg"
+	    fi
+	   ;;
+    esac
+done
+
+$verbose && set -x
+$cc -o "$output.tmp" -Wl,-Ur -nostdlib $stage1_args
+$cc -o "$output" "$output.tmp" $stage2_args
+rm -f $output.tmp
