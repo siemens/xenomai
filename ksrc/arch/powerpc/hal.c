8fa19a65d494ca5db95bad04ead0cfb08d36bf27
Philippe Gerum <rpm@xenomai.org>
2010-01-11 17:27:05 +0100

    powerpc: use rthal_irqdesc_lock/unlock()

--- ksrc/arch/powerpc/hal.c
+++ ksrc/arch/powerpc/hal.c
@@ -346,14 +346,14 @@ int rthal_irq_host_request(unsigned irq,
 	    rthal_irq_descp(irq) == NULL)
 		return -EINVAL;
 
-	spin_lock_irqsave(&rthal_irq_descp(irq)->lock, flags);
+	rthal_irqdesc_lock(irq, flags);
 
 	if (rthal_linux_irq[irq].count++ == 0 && rthal_irq_descp(irq)->action) {
 		rthal_linux_irq[irq].flags = rthal_irq_descp(irq)->action->flags;
 		rthal_irq_descp(irq)->action->flags |= IRQF_SHARED;
 	}
 
-	spin_unlock_irqrestore(&rthal_irq_descp(irq)->lock, flags);
+	rthal_irqdesc_unlock(irq, flags);
 
 	return request_irq(irq, handler, IRQF_SHARED, name, dev_id);
 }
@@ -369,12 +369,12 @@ int rthal_irq_host_release(unsigned irq, void *dev_id)
 
 	free_irq(irq, dev_id);
 
-	spin_lock_irqsave(&rthal_irq_descp(irq)->lock, flags);
+	rthal_irqdesc_lock(irq, flags);
 
 	if (--rthal_linux_irq[irq].count == 0 && rthal_irq_descp(irq)->action)
 		rthal_irq_descp(irq)->action->flags = rthal_linux_irq[irq].flags;
 
-	spin_unlock_irqrestore(&rthal_irq_descp(irq)->lock, flags);
+	rthal_irqdesc_unlock(irq, flags);
 
 	return 0;
 }
