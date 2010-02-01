37b5c807e1bb4ac9f4024734349d9939a2fd357f
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2010-02-02 00:46:32 +0100

    skins: avoid extern weak declarations
    They cause screwed up binaries. The weak attribute is for the linker
    anyway, so does not need to be present at the symbol declaration.

--- include/asm-generic/bits/sigshadow.h
+++ include/asm-generic/bits/sigshadow.h
@@ -4,10 +4,10 @@
 #include <pthread.h>
 #include <signal.h>
 
-extern pthread_once_t __attribute__((weak)) xeno_sigshadow_installed;
-extern struct sigaction __attribute__((weak)) xeno_saved_sigshadow_action;
+extern pthread_once_t xeno_sigshadow_installed;
+extern struct sigaction xeno_saved_sigshadow_action;
 
-void __attribute__((weak)) xeno_sigshadow_install(void);
+void xeno_sigshadow_install(void);
 
 static inline void sigshadow_install_once(void)
 {
