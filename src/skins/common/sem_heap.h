70354bbcffeafafaafbefc3b42dd99fbc95bb537
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2010-04-01 22:00:23 +0200

    common: unconditionnaly map the semaphore heaps in user-space.
    This is needed for the vdso feature check, now mandatory, even without the
    FASTSYNCH feature.

--- src/skins/common/sem_heap.h
+++ src/skins/common/sem_heap.h
@@ -3,10 +3,6 @@
 
 #include <xeno_config.h>
 
-#ifdef CONFIG_XENO_FASTSYNCH
 void xeno_init_sem_heaps(void);
-#else /* !CONFIG_XENO_FASTSYNCH */
-#define xeno_init_sem_heaps()
-#endif /* !CONFIG_XENO_FASTSYNCH */
 
 #endif /* XENO_SEM_HEAP_H */
