siemens/xenomai

commit 695f3d0eaa18dd0c807f7a8b5e5b5b05d302b080

Shorten uninterruptible section within shadow relaxing path

ksrc/nucleus/shadow.c
@@ -733,33 +733,28 @@ static void lostage_handler(void *cookie)
 
 		switch (rq->req[reqnum].type) {
 		case LO_UNMAP_REQ:
-
 			xnshadow_dereference_skin(rq->req[reqnum].arg);
 
 			/* fall through */
 		case LO_WAKEUP_REQ:
-
-			/* We need to downgrade the root thread
-			   priority whenever the APC runs over a
-			   non-shadow, so that the temporary boost we
-			   applied in xnshadow_relax() is not
-			   spuriously inherited by the latter until
-			   the relaxed shadow actually resumes in
-			   secondary mode. */
-
+			/*
+			 * We need to downgrade the root thread
+			 * priority whenever the APC runs over a
+			 * non-shadow, so that the temporary boost we
+			 * applied in xnshadow_relax() is not
+			 * spuriously inherited by the latter until
+			 * the relaxed shadow actually resumes in
+			 * secondary mode.
+			 */
 			rpi_clear_local(xnshadow_thread(current));
+			xnpod_schedule();
 
 			/* fall through */
 		case LO_START_REQ:
-
 			wake_up_process(p);
-
-			xnpod_schedule();
-
 			break;
 
 		case LO_SIGTHR_REQ:
-
 			xnshadow_sig_demux(rq->req[reqnum].arg, sig, arg);
 			if (sig == SIGSHADOW) {
 				siginfo_t si;
@@ -773,7 +768,6 @@ static void lostage_handler(void *cookie)
 			break;
 
 		case LO_SIGGRP_REQ:
-
 			sig = rq->req[reqnum].arg;
 			kill_proc(p->pid, sig, 1);
 			break;
@@ -783,10 +777,8 @@ static void lostage_handler(void *cookie)
 
 static void schedule_linux_call(int type, struct task_struct *p, int arg)
 {
-	/* Do _not_ use smp_processor_id() here so we don't trigger Linux
-	   preemption debug traps inadvertently (see lib/smp_processor_id.c). */
 	int cpu = rthal_processor_id(), reqnum;
-	struct __lostagerq *rq = &lostagerq[cpu];
+	struct __lostagerq *rq;
 	spl_t s;
 
 	XENO_ASSERT(NUCLEUS, p,
@@ -797,17 +789,15 @@ static void schedule_linux_call(int type, struct task_struct *p, int arg)
 
 	splhigh(s);
 
+	rq = &lostagerq[cpu];
 	reqnum = rq->in;
-
-	if (XENO_DEBUG(NUCLEUS) &&
-	    ((reqnum + 1) & (LO_MAX_REQUESTS - 1)) == rq->out)
+	rq->in = (reqnum + 1) & (LO_MAX_REQUESTS - 1);
+	if (XENO_DEBUG(NUCLEUS) && rq->in == rq->out)
 	    xnpod_fatal("lostage queue overflow on CPU %d! "
 			"Increase LO_MAX_REQUESTS", cpu);
-
 	rq->req[reqnum].type = type;
 	rq->req[reqnum].task = p;
 	rq->req[reqnum].arg = arg;
-	rq->in = (reqnum + 1) & (LO_MAX_REQUESTS - 1);
 	
 	splexit(s);
 
@@ -1060,16 +1050,11 @@ void xnshadow_relax(int notify)
 	trace_mark(xn_nucleus, shadow_gorelax, "thread %p thread_name %s",
 		  thread, xnthread_name(thread));
 
-	splhigh(s);
-
-	schedule_linux_call(LO_WAKEUP_REQ, current, 0);
-
 	rpi_push(thread->sched, thread);
-
+	splhigh(s);
+	schedule_linux_call(LO_WAKEUP_REQ, current, 0);
 	clear_task_nowakeup(current);
-
 	xnpod_suspend_thread(thread, XNRELAX, XN_INFINITE, XN_RELATIVE, NULL);
-
 	splexit(s);
 
 	if (XENO_DEBUG(NUCLEUS) && rthal_current_domain != rthal_root_domain)

ksrc/nucleus/pod.c
@@ -1373,17 +1373,17 @@ void xnpod_suspend_thread(xnthread_t *thread, xnflags_t mask,
 		/*
 		 * If attempting to suspend a runnable (shadow) thread
 		 * which has received a Linux signal, just raise the
-		 * break condition and return immediately. Note: a
-		 * relaxed shadow never has the KICKED bit set, so
-		 * that xnshadow_relax() is never prevented from
-		 * blocking the current thread.
-		 */
-		if (xnthread_test_info(thread, XNKICKED)) {
-			XENO_ASSERT(NUCLEUS, (mask & XNRELAX) == 0,
-				    xnpod_fatal("Relaxing a kicked thread"
-						"(thread=%s, mask=%lx)?!",
-						thread->name, mask);
-				);
+		 * break condition and return immediately.  We may end
+		 * up suspending a kicked thread that has been
+		 * preempted on its relaxing path, which is a
+		 * perfectly valid situation: we just ignore the
+		 * signal notification in primary mode, and rely on
+		 * the wakeup call pending for that task in the root
+		 * context, to collect and act upon the pending Linux
+		 * signal.
+ 		 */
+		if ((mask & XNRELAX) == 0 &&
+		    xnthread_test_info(thread, XNKICKED)) {
 			xnthread_clear_info(thread, XNRMID | XNTIMEO);
 			xnthread_set_info(thread, XNBREAK);
 			goto unlock_and_exit;
@@ -1432,12 +1432,19 @@ void xnpod_suspend_thread(xnthread_t *thread, xnflags_t mask,
 		nkpod->schedhook(thread, mask);
 #endif /* __XENO_SIM__ */
 
-	if (thread == sched->curr)
+	if (thread == sched->curr) {
+		spl_t ignored;
 		/*
 		 * If the thread is runnning on another CPU,
-		 * xnpod_schedule will just trigger the IPI.
+		 * xnpod_schedule will just trigger the IPI. We
+		 * release the interrupts shortly before rescheduling,
+		 * to pull any pending interrupt that may reorder the
+		 * scheduling state first.
 		 */
+		xnlock_clear_irqon(&nklock);
 		xnpod_schedule();
+		xnlock_get_irqsave(&nklock, ignored);
+	}
 #ifdef CONFIG_XENO_OPT_PERVASIVE
 	/*
 	 * Ok, this one is an interesting corner case, which requires

commit d3242401b8e1cf2c28822f801b681194243b4394

nucleus/shadow: fix RPI race in xnshadow_relax() introduced by 695f3d0
This patch fixes a race in the RPI management code which may cause the
root thread to remain priority-boosted indefinitely. The related bug
was introduced in the 2.5 timeframe by commit 695f3d0 (aka killer
optimization).

The faulty code fragment from xnshadow_relax(), is this one:

	rpi_push(thread->sched, thread);
	splhigh(s);
	schedule_linux_call(LO_WAKEUP_REQ, current, 0);

The sequence of events leading to the bug is as follows:

- a Xenomai thread running in primary mode enters xnshadow_relax(), to
  move to the Linux domain; because this thread runs under the control
  of the Xenomai scheduler at this point, it is blocked linux-wise
  (TASK_INTERRUPTIBLE).

- a real-time timer tick preempts the thread and switches it out,
  right after it linked itself to the local RPI queue calling
  rpi_push(), but before it had a chance to send an asynchronous wake
  up request for itself to the linux kernel
  (i.e. schedule_linux_call(LO_WAKEUP_REQ...).

As a consequence of this, a blocked shadow linux-wise was introduced
into the RPI queue, with no later opportunity to leave it,
indefinitely preventing the root thread to be demoted to its regular
idle priority.

That bug is easily reproduced by running the latency test concurrently
to the mutex-torture-native test.

The fix consists of making the two operations described above, belong
to a single atomic sequence, by proper interrupt masking.

ksrc/nucleus/shadow.c
@@ -1148,8 +1148,8 @@ void xnshadow_relax(int notify, int reason)
 	trace_mark(xn_nucleus, shadow_gorelax, "thread %p thread_name %s",
 		  thread, xnthread_name(thread));
 
-	rpi_push(thread->sched, thread);
 	splhigh(s);
+	rpi_push(thread->sched, thread);
 	schedule_linux_call(LO_WAKEUP_REQ, current, 0);
 	clear_task_nowakeup(current);
 	xnpod_suspend_thread(thread, XNRELAX, XN_INFINITE, XN_RELATIVE, NULL);