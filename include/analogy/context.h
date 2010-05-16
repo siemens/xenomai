e1e19708538fed4b2d105db7300ac6822dfae3e4
Alexis Berlemont <alexis.berlemont@gmail.com>
2010-05-16 23:11:47 +0200

    analogy: change the context's role (broken)

--- include/analogy/context.h
+++ include/analogy/context.h
@@ -25,29 +25,20 @@
 
 #if defined(__KERNEL__) && !defined(DOXYGEN_CPP)
 
-#include <analogy/os_facilities.h>
-
 struct a4l_device;
+struct a4l_buffer;
 
-struct a4l_context {
-
-	/* This field is redundant with the following parameters;
-	   setting it at the head of the structure may save 
-	   useless operations */
-	struct a4l_device *dev;
+struct a4l_device_context {
+	/* No need to hold user_info, thanks to container_of, we could
+	   get it back */
 	rtdm_user_info_t *user_info;
-	struct rtdm_dev_context *rtdm_cxt;
+
+	struct a4l_device *dev; /* Which is retrieved thanks to minor
+				   at open time */
+
+	struct buffer buffer; /* The buffer field is extracted from
+				 the transfer structure */
 };
-typedef struct a4l_context a4l_cxt_t;
-
-#define a4l_get_minor(x) ((x)->rtdm_cxt->device->device_id)
-
-#define a4l_init_cxt(c, u, x)			\
-    {							\
-	(x)->rtdm_cxt = c;				\
-	(x)->user_info = u;				\
-	(x)->dev = NULL;				\
-    }
 
 #endif /* __KERNEL__ && !DOXYGEN_CPP */
 
