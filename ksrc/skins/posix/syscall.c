e385b4b8b7dc020aa5884c79365c903f5a78c49d
Philippe Gerum <rpm@xenomai.org>
2010-05-25 17:07:23 +0200

    posix: fix build w/o CONFIG_PROC_FS

--- ksrc/skins/posix/syscall.c
+++ ksrc/skins/posix/syscall.c
@@ -20,6 +20,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <linux/err.h>
 #include <asm/xenomai/wrappers.h>
 #include <nucleus/jhash.h>
 #include <nucleus/ppd.h>
