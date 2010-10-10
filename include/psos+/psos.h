cc063bf26f600072afaf50e5a22c538f4ce533ce
Philippe Gerum <rpm@xenomai.org>
2010-10-10 11:52:05 +0200

    psos: map T_SHADOW on XNSHADOW

--- include/psos+/psos.h
+++ include/psos+/psos.h
@@ -58,7 +58,7 @@
 #define T_FPU         0x0002
 
 /* Those bits are Xenomai-specific. */
-#define T_SHADOW      0x8000
+#define T_SHADOW      XNSHADOW
 #define T_TRAPSW      XNTRAPSW
 #define T_RPIOFF      XNRPIOFF
 
