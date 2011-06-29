6bb1f1d6241667107ae53ddf6b504e5cac6c92f1
Jan Kiszka <jan.kiszka@siemens.com>
2011-06-29 13:35:11 +0200

    nucleus: Remove debug printk
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- ksrc/nucleus/shadow.c
+++ ksrc/nucleus/shadow.c
@@ -2162,7 +2162,6 @@ static void *xnshadow_sys_event(int event, void *data)
 		return &p->ppd;
 
 	case XNSHADOW_CLIENT_DETACH:
-		printk("sys detach !\n");
 		p = ppd2sys(data);
 		xnheap_destroy_mapped(&p->sem_heap, post_ppd_release, NULL);
 
