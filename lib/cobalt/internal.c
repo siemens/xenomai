407c99d91d3877bb3bcc4a3ee52123e561f0a778
Philippe Gerum <rpm@xenomai.org>
2011-10-06 12:37:17 +0200

    lib/cobalt: introduce Xenomai-private services, __cobalt_thread_harden()

---
+++ lib/cobalt/internal.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2011 Philippe Gerum <rpm@xenomai.org>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <cobalt/syscall.h>
+#include <asm-generic/bits/current.h>
+
+extern int __pse51_muxid;
+
+void __cobalt_thread_harden(void)
+{
+	unsigned long status = xeno_get_current_mode();
+
+	/* non-RT shadows are NOT allowed to force primary mode. */
+	if ((status & (XNRELAX|XNOTHER)) == XNRELAX)
+		XENOMAI_SYSCALL1(__xn_sys_migrate, XENOMAI_XENO_DOMAIN);
+}
---
+++ lib/cobalt/internal.h
@@ -0,0 +1,8 @@
+#ifndef XENO_COBALT_INTERNAL_H
+#define XENO_COBALT_INTERNAL_H
+
+#include <xeno_config.h>
+
+void __cobalt_thread_harden(void);
+
+#endif /* XENO_COBALT_INTERNAL_H */
