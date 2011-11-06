2e84939fbef2b24a895fe1b29f343efbaacc6a45
Philippe Gerum <rpm@xenomai.org>
2011-11-06 18:48:48 +0100

    alchemy/testsuite: more task related tests

---
+++ lib/alchemy/testsuite/task-3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <copperplate/init.h>
+#include <copperplate/traceobj.h>
+#include <alchemy/task.h>
+
+static struct traceobj trobj;
+
+static RT_TASK t_a, t_b;
+
+int main(int argc, char *argv[])
+{
+	RT_TASK t;
+	int ret;
+
+	copperplate_init(argc, argv);
+
+	traceobj_init(&trobj, argv[0], 0);
+
+	ret = rt_task_create(&t_a, "taskA", 0,  20, 0);
+	traceobj_assert(&trobj, ret == 0);
+
+	ret = rt_task_create(&t_b, "taskB", 0,  21, 0);
+	traceobj_assert(&trobj, ret == 0);
+
+	ret = rt_task_bind(&t, "taskA", TM_NONBLOCK);
+	traceobj_assert(&trobj, ret == 0);
+	traceobj_assert(&trobj, rt_task_same(&t, &t_a));
+
+	ret = rt_task_bind(&t, "taskB", TM_NONBLOCK);
+	traceobj_assert(&trobj, ret == 0);
+	traceobj_assert(&trobj, rt_task_same(&t, &t_b));
+
+	ret = rt_task_delete(&t_a);
+	traceobj_assert(&trobj, ret == 0);
+	ret = rt_task_bind(&t, "taskA", TM_NONBLOCK);
+	traceobj_assert(&trobj, ret == -EWOULDBLOCK);
+
+	ret = rt_task_delete(&t_b);
+	traceobj_assert(&trobj, ret == 0);
+	ret = rt_task_bind(&t, "taskB", TM_NONBLOCK);
+	traceobj_assert(&trobj, ret == -EWOULDBLOCK);
+
+	exit(0);
+}
