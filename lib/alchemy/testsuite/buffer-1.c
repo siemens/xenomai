ad27543604875164bc3deed09944245c12d48891
Philippe Gerum <rpm@xenomai.org>
2011-12-18 22:30:01 +0100

    alchemy/testsuite: buffer: test deletion during I/O operations

--- lib/alchemy/testsuite/buffer-1.c
+++ lib/alchemy/testsuite/buffer-1.c
@@ -28,6 +28,9 @@ static void foreground_task(void *arg)
 		memset(buf,0, sizeof(buf));
 		ret = rt_buffer_read(&buffer, buf, 2, TM_INFINITE);
 		switch (ret) {
+		case -EINVAL:
+		case -EIDRM:	/* Fine, deleted. */
+			goto out;
 		case 1:
 			traceobj_assert(&trobj, buf[0] == ((n / 2) % 26) + 'A');
 			break;
@@ -39,7 +42,7 @@ static void foreground_task(void *arg)
 		}
 		n++;
 	}
-
+out:
 	traceobj_exit(&trobj);
 }
 
@@ -53,12 +56,16 @@ static void background_task(void *arg)
 
 	for (;;) {
 		ret = rt_buffer_write(&buffer, &c, 1, TM_INFINITE);
+		if (ret == -EINVAL || ret == -EIDRM)
+			break;
 		traceobj_assert(&trobj, ret == 1);
 		c++;
 		if (c > 'Z')
 			c = 'A';
 		sprintf(s, "%.2d", 11 * n);
 		ret = rt_buffer_write(&buffer, s, 2, TM_INFINITE);
+		if (ret == -EINVAL || ret == -EIDRM)
+			break;
 		traceobj_assert(&trobj, ret == 2);
 		n = (n + 1) % 10;
 	}
@@ -93,12 +100,6 @@ int main(int argc, char *const argv[])
 	traceobj_assert(&trobj, ret == 0);
 
 	ret = rt_task_sleep(1500000000ULL);
-	traceobj_assert(&trobj, ret == 0);
-
-	ret = rt_task_delete(&t_bgnd);
-	traceobj_assert(&trobj, ret == 0);
-
-	ret = rt_task_delete(&t_fgnd);
 	traceobj_assert(&trobj, ret == 0);
 
 	ret = rt_buffer_delete(&buffer);
