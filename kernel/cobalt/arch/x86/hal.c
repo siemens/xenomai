d7a6811050b95c5d58e581bfd018db8aa461178b
Philippe Gerum <rpm@xenomai.org>
2012-01-01 17:33:19 +0100

    hal/x86: unify 32/64 bit support

--- kernel/cobalt/arch/x86/hal.c
+++ kernel/cobalt/arch/x86/hal.c
@@ -3,16 +3,9 @@
  *   @file
  *
  *   Adeos-based Real-Time Abstraction Layer for x86.
+ *   Common code of i386 and x86_64.
  *
- *   Inspired from original RTAI/x86 HAL interface: \n
- *   Copyright &copy; 2000 Paolo Mantegazza, \n
- *   Copyright &copy; 2000 Steve Papacharalambous, \n
- *   Copyright &copy; 2000 Stuart Hughes, \n
- *
- *   RTAI/x86 rewrite over Adeos: \n
- *   Copyright &copy; 2002-2007 Philippe Gerum.
- *   SMI workaround: \n
- *   Copyright &copy; 2004 Gilles Chanteperdrix.
+ *   Copyright (C) 2007-2012 Philippe Gerum.
  *
  *   Xenomai is free software; you can redistribute it and/or
  *   modify it under the terms of the GNU General Public License as
@@ -29,27 +22,169 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  *   02111-1307, USA.
- *
- *   i386-specific HAL services.
  */
-#include <linux/version.h>
-#include <linux/slab.h>
-#include <linux/errno.h>
+
+/**
+ * @addtogroup hal
+ *
+ *@{*/
+
 #include <linux/module.h>
-#include <linux/console.h>
-#include <linux/bitops.h>
-#include <asm/system.h>
-#include <asm/hardirq.h>
-#include <asm/desc.h>
-#include <asm/io.h>
-#include <asm/delay.h>
-#include <asm/uaccess.h>
-#include <asm/unistd.h>
+#include <linux/ipipe_tickdev.h>
 #include <asm/xenomai/hal.h>
-#include <stdarg.h>
-#include <asm/nmi.h>
+
+enum rthal_ktimer_mode rthal_ktimer_saved_mode;
 
 #ifdef CONFIG_X86_LOCAL_APIC
+
+static volatile int sync_op;
+
+#define RTHAL_SET_ONESHOT_XENOMAI	1
+#define RTHAL_SET_ONESHOT_LINUX		2
+#define RTHAL_SET_PERIODIC		3
+
+static void critical_sync(void)
+{
+	if (!rthal_cpu_supported(ipipe_processor_id()))
+		return;
+	switch (sync_op) {
+	case RTHAL_SET_ONESHOT_XENOMAI:
+		rthal_setup_oneshot_apic(RTHAL_APIC_TIMER_VECTOR);
+		break;
+
+	case RTHAL_SET_ONESHOT_LINUX:
+		rthal_setup_oneshot_apic(LOCAL_TIMER_VECTOR);
+		/* We need to keep the timing cycle alive for the kernel. */
+		ipipe_raise_irq(RTHAL_HOST_TICK_IRQ);
+		break;
+
+	case RTHAL_SET_PERIODIC:
+		rthal_setup_periodic_apic(RTHAL_APIC_ICOUNT, LOCAL_TIMER_VECTOR);
+		break;
+	}
+}
+
+static void rthal_timer_set_oneshot(int rt_mode)
+{
+	unsigned long flags;
+
+	flags = ipipe_critical_enter(critical_sync);
+	if (rt_mode) {
+		sync_op = RTHAL_SET_ONESHOT_XENOMAI;
+		if (rthal_cpu_supported(ipipe_processor_id()))
+			rthal_setup_oneshot_apic(RTHAL_APIC_TIMER_VECTOR);
+		if (rthal_ktimer_saved_mode != KTIMER_MODE_UNUSED)
+			__ipipe_hrtimer_irq = RTHAL_TIMER_IRQ;
+	} else {
+		sync_op = RTHAL_SET_ONESHOT_LINUX;
+		if (rthal_cpu_supported(ipipe_processor_id()))
+			rthal_setup_oneshot_apic(LOCAL_TIMER_VECTOR);
+		__ipipe_hrtimer_irq = RTHAL_HOST_TICK_IRQ;
+		/* We need to keep the timing cycle alive for the kernel. */
+		ipipe_raise_irq(RTHAL_HOST_TICK_IRQ);
+	}
+	ipipe_critical_exit(flags);
+}
+
+static void rthal_timer_set_periodic(void)
+{
+	unsigned long flags;
+
+	flags = ipipe_critical_enter(critical_sync);
+	sync_op = RTHAL_SET_PERIODIC;
+	if (rthal_cpu_supported(ipipe_processor_id()))
+		rthal_setup_periodic_apic(RTHAL_APIC_ICOUNT, LOCAL_TIMER_VECTOR);
+	__ipipe_hrtimer_irq = RTHAL_HOST_TICK_IRQ;
+	ipipe_critical_exit(flags);
+}
+
+static int cpu_timers_requested;
+
+int rthal_timer_request(
+	void (*tick_handler)(void),
+	void (*mode_emul)(enum clock_event_mode mode,
+			  struct clock_event_device *cdev),
+	int (*tick_emul)(unsigned long delay,
+			 struct clock_event_device *cdev),
+	int cpu)
+{
+	unsigned long dummy, *tmfreq = &dummy;
+	int tickval, ret, res;
+
+	if (cpu_timers_requested == 0) {
+		ret = ipipe_request_irq(&rthal_archdata.domain,
+					RTHAL_APIC_TIMER_IPI,
+					(ipipe_irq_handler_t)tick_handler,
+					NULL, NULL);
+		if (ret)
+			return ret;
+	}
+
+	/* This code works both for UP+LAPIC and SMP configurations. */
+
+	if (rthal_timerfreq_arg == 0)
+		tmfreq = &rthal_archdata.timer_freq;
+
+	res = ipipe_request_tickdev("lapic", mode_emul, tick_emul, cpu,
+				    tmfreq);
+	switch (res) {
+	case CLOCK_EVT_MODE_PERIODIC:
+		/* oneshot tick emulation callback won't be used, ask
+		 * the caller to start an internal timer for emulating
+		 * a periodic tick. */
+		tickval = 1000000000UL / HZ;
+		break;
+
+	case CLOCK_EVT_MODE_ONESHOT:
+		/* oneshot tick emulation */
+		tickval = 1;
+		break;
+
+	case CLOCK_EVT_MODE_UNUSED:
+		/* we don't need to emulate the tick at all. */
+		tickval = 0;
+		break;
+
+	case CLOCK_EVT_MODE_SHUTDOWN:
+		res = -ENODEV;
+		/* fall through */
+
+	default:
+		if (cpu_timers_requested == 0)
+			ipipe_free_irq(&rthal_archdata.domain,
+				       RTHAL_APIC_TIMER_IPI);
+		return res;
+	}
+	rthal_ktimer_saved_mode = res;
+
+	/*
+	 * The rest of the initialization should only be performed
+	 * once by a single CPU.
+	 */
+	if (cpu_timers_requested++ == 0)
+		rthal_timer_set_oneshot(1);
+
+	return tickval;
+}
+
+void rthal_timer_release(int cpu)
+{
+	ipipe_release_tickdev(cpu);
+
+	/*
+	 * The rest of the cleanup work should only be performed once
+	 * by the last releasing CPU.
+	 */
+	if (--cpu_timers_requested > 0)
+		return;
+
+	if (rthal_ktimer_saved_mode == KTIMER_MODE_PERIODIC)
+		rthal_timer_set_periodic();
+	else if (rthal_ktimer_saved_mode == KTIMER_MODE_ONESHOT)
+		rthal_timer_set_oneshot(0);
+
+	ipipe_free_irq(&rthal_archdata.domain, RTHAL_APIC_TIMER_IPI);
+}
 
 unsigned long rthal_timer_calibrate(void)
 {
@@ -79,9 +214,25 @@ unsigned long rthal_timer_calibrate(void)
 	return rthal_ulldiv(dt, 20, NULL);
 }
 
+static inline int check_x86_features(void)
+{
+	/*
+	 * If support for LAPIC was built in, then we want it enabled.
+	 */
+	if (!boot_cpu_has(X86_FEATURE_APIC)) {
+		printk("Xenomai: Local APIC absent or disabled!\n"
+		       "         Disable APIC support or pass \"lapic=1\" as bootparam.\n");
+		rthal_smi_restore();
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
 #else /* !CONFIG_X86_LOCAL_APIC */
 
-extern enum rthal_ktimer_mode rthal_ktimer_saved_mode;
+#include <asm/io.h>
+#include <asm/delay.h>
 
 unsigned long rthal_timer_calibrate(void)
 {
@@ -235,18 +386,34 @@ void rthal_timer_release(int cpu)
 		ipipe_raise_irq(RTHAL_TIMER_IRQ);
 }
 
+static inline int check_x86_features(void)
+{
+	return 0;
+}
+
 #endif /* !CONFIG_X86_LOCAL_APIC */
+
+void rthal_timer_notify_switch(enum clock_event_mode mode,
+			       struct clock_event_device *cdev)
+{
+	if (ipipe_processor_id() > 0)
+		/*
+		 * We assume all CPUs switch the same way, so we only
+		 * track mode switches from the boot CPU.
+		 */
+		return;
+
+	rthal_ktimer_saved_mode = mode;
+}
+EXPORT_SYMBOL_GPL(rthal_timer_notify_switch);
 
 int rthal_arch_init(void)
 {
-#ifdef CONFIG_X86_LOCAL_APIC
-	if (!boot_cpu_has(X86_FEATURE_APIC)) {
-		printk("Xenomai: Local APIC absent or disabled!\n"
-		       "         Disable APIC support or pass \"lapic=1\" as bootparam.\n");
-		rthal_smi_restore();
-		return -ENODEV;
-	}
-#endif /* CONFIG_X86_LOCAL_APIC */
+	int ret;
+
+	ret = check_x86_features();
+	if (ret)
+		return ret;
 
 	/* FIXME: 4Ghz barrier is close... */
 	if (rthal_clockfreq_arg == 0)
@@ -254,11 +421,11 @@ int rthal_arch_init(void)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(rthal_arch_init);
 
 void rthal_arch_cleanup(void)
 {
-	printk(KERN_INFO "Xenomai: hal/i386 stopped.\n");
 }
-
-EXPORT_SYMBOL_GPL(rthal_arch_init);
 EXPORT_SYMBOL_GPL(rthal_arch_cleanup);
+
+/*@}*/
