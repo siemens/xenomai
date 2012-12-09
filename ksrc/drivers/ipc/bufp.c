ec734d3c5b676fe7d0a0a11ed1ad36d9e0832f14
Philippe Gerum <rpm@xenomai.org>
2012-12-09 11:14:25 +0100

    drivers/ipc: fix section mismatches

--- ksrc/drivers/ipc/bufp.c
+++ ksrc/drivers/ipc/bufp.c
@@ -1051,7 +1051,7 @@ static int bufp_ioctl(struct rtipc_private *priv,
 	return __bufp_ioctl(priv, user_info, request, arg);
 }
 
-static int __init bufp_init(void)
+static int bufp_init(void)
 {
 	portmap = xnmap_create(CONFIG_XENO_OPT_BUFP_NRPORT, 0, 0);
 	if (portmap == NULL)
