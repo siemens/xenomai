1d0068e7fe30ff0d262f44bf1faa224e6acf4390
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2012-12-10 01:43:07 +0100

    arm/mayday: fix mayday with applications compiled in thumb mode

--- include/asm-arm/bits/shadow.h
+++ include/asm-arm/bits/shadow.h
@@ -296,6 +296,11 @@ static inline void xnarch_handle_mayday(struct xnarchtcb *tcb,
 #ifdef CONFIG_XENO_ARM_EABI
 	tcb->mayday.r7 = regs->ARM_r7;
 #endif
+#ifdef CONFIG_ARM_THUMB
+	/* The code on the mayday page must be run in ARM mode */
+	tcb->mayday.psr = regs->ARM_cpsr;
+	regs->ARM_cpsr &= ~PSR_T_BIT;
+#endif
 	regs->ARM_pc = tramp;
 }
 
@@ -306,6 +311,9 @@ static inline void xnarch_fixup_mayday(struct xnarchtcb *tcb,
 	regs->ARM_r0 = tcb->mayday.r0;
 #ifdef CONFIG_XENO_ARM_EABI
 	regs->ARM_r7 = tcb->mayday.r7;
+#endif
+#ifdef CONFIG_ARM_THUMB
+	regs->ARM_cpsr = tcb->mayday.psr;
 #endif
 }
 
