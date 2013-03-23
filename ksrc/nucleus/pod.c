926f4fbc2e38c198eefda6f0a98a278588d421db
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2013-03-23 21:45:39 +0100

    nucleus: do not start host timer emulation if host timer is not shared

--- ksrc/nucleus/pod.c
+++ ksrc/nucleus/pod.c
@@ -2754,7 +2754,7 @@ int xnpod_enable_timesource(void)
 
 		if (htickval > 1)
 			xntimer_start(&sched->htimer, htickval, htickval, XN_RELATIVE);
-		else
+		else if (htickval == 1)
 			xntimer_start(&sched->htimer, 0, 0, XN_RELATIVE);
 
 #if defined(CONFIG_XENO_OPT_WATCHDOG)
