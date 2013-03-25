1646bd4d24e1dc329bbaa63f716c7ac3312aefe4
Philippe Gerum <rpm@xenomai.org>
2013-03-25 16:55:37 +0100

    psos/testsuite: add task creation+deletion loop test

---
+++ lib/psos/testsuite/task-9.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <copperplate/init.h>
+#include <copperplate/traceobj.h>
+#include <psos/psos.h>
+
+#define MAX_PRIO 95
+
+static struct traceobj trobj;
+
+static void test_task(u_long a0, u_long a1, u_long a2, u_long a3)
+{
+	traceobj_enter(&trobj);
+	tm_wkafter(1000000);
+	traceobj_exit(&trobj);
+}
+
+static void root_task(u_long a0, u_long a1, u_long a2, u_long a3)
+{
+	u_long args[] = { 1, 2, 3, 4 }, ret, tid;
+	int n;
+
+	traceobj_enter(&trobj);
+
+	for (n = 0; n < 512; n++) {
+		ret = t_create ("TEST", (n % MAX_PRIO) + 2,100000, 0, 0, &tid);
+		traceobj_assert(&trobj, ret == SUCCESS);
+		ret = t_start(tid, T_PREEMPT, test_task, args);
+		traceobj_assert(&trobj, ret == SUCCESS);
+		ret = t_delete(tid);
+		traceobj_assert(&trobj, ret == SUCCESS);
+	}
+
+	traceobj_exit(&trobj);
+}
+
+int main(int argc, char *const argv[])
+{
+	u_long args[] = { 1, 2, 3, 4 }, ret, tid;
+
+	copperplate_init(&argc, &argv);
+
+	traceobj_init(&trobj, argv[0], 0);
+
+	ret = t_create("root", 3, 0, 0, 0, &tid);
+	traceobj_assert(&trobj, ret == SUCCESS);
+
+	ret = t_start(tid, 0, root_task, args);
+	traceobj_assert(&trobj, ret == SUCCESS);
+
+	traceobj_join(&trobj);
+
+	exit(0);
+}
