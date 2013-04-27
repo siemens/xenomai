9a774c8ed832a9ab1260455b54aec4ef55321a17
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2013-04-27 22:52:46 +0200

    skins: move xntrace API out-of-line in libxenomai library

--- include/nucleus/trace.h
+++ include/nucleus/trace.h
@@ -51,48 +51,21 @@
 
 #include <asm/xenomai/syscall.h>
 
-static inline int xntrace_max_begin(unsigned long v)
-{
-	return XENOMAI_SYSCALL2(__xn_sys_trace, __xntrace_op_max_begin, v);
-}
+int xntrace_max_begin(unsigned long v);
 
-static inline int xntrace_max_end(unsigned long v)
-{
-	return XENOMAI_SYSCALL2(__xn_sys_trace, __xntrace_op_max_end, v);
-}
+int xntrace_max_end(unsigned long v);
 
-static inline int xntrace_max_reset(void)
-{
-	return XENOMAI_SYSCALL1(__xn_sys_trace, __xntrace_op_max_reset);
-}
+int xntrace_max_reset(void);
 
-static inline int xntrace_user_start(void)
-{
-	return XENOMAI_SYSCALL1(__xn_sys_trace, __xntrace_op_user_start);
-}
+int xntrace_user_start(void);
 
-static inline int xntrace_user_stop(unsigned long v)
-{
-	return XENOMAI_SYSCALL2(__xn_sys_trace, __xntrace_op_user_stop, v);
-}
+int xntrace_user_stop(unsigned long v);
 
-static inline int xntrace_user_freeze(unsigned long v, int once)
-{
-	return XENOMAI_SYSCALL3(__xn_sys_trace, __xntrace_op_user_freeze,
-				v, once);
-}
+int xntrace_user_freeze(unsigned long v, int once);
 
-static inline int xntrace_special(unsigned char id, unsigned long v)
-{
-	return XENOMAI_SYSCALL3(__xn_sys_trace, __xntrace_op_special, id, v);
-}
+int xntrace_special(unsigned char id, unsigned long v);
 
-static inline int xntrace_special_u64(unsigned char id, unsigned long long v)
-{
-	return XENOMAI_SYSCALL4(__xn_sys_trace, __xntrace_op_special_u64, id,
-				(unsigned long)(v >> 32),
-				(unsigned long)(v & 0xFFFFFFFF));
-}
+int xntrace_special_u64(unsigned char id, unsigned long long v);
 
 #endif /* defined(__KERNEL__) || defined(__XENO_SIM__) */
 
---
+++ src/skins/common/trace.c
@@ -0,0 +1,45 @@
+#include <nucleus/trace.h>
+
+int xntrace_max_begin(unsigned long v)
+{
+	return XENOMAI_SYSCALL2(__xn_sys_trace, __xntrace_op_max_begin, v);
+}
+
+int xntrace_max_end(unsigned long v)
+{
+	return XENOMAI_SYSCALL2(__xn_sys_trace, __xntrace_op_max_end, v);
+}
+
+int xntrace_max_reset(void)
+{
+	return XENOMAI_SYSCALL1(__xn_sys_trace, __xntrace_op_max_reset);
+}
+
+int xntrace_user_start(void)
+{
+	return XENOMAI_SYSCALL1(__xn_sys_trace, __xntrace_op_user_start);
+}
+
+int xntrace_user_stop(unsigned long v)
+{
+	return XENOMAI_SYSCALL2(__xn_sys_trace, __xntrace_op_user_stop, v);
+}
+
+int xntrace_user_freeze(unsigned long v, int once)
+{
+	return XENOMAI_SYSCALL3(__xn_sys_trace, __xntrace_op_user_freeze,
+				v, once);
+}
+
+int xntrace_special(unsigned char id, unsigned long v)
+{
+	return XENOMAI_SYSCALL3(__xn_sys_trace, __xntrace_op_special, id, v);
+}
+
+int xntrace_special_u64(unsigned char id, unsigned long long v)
+{
+	return XENOMAI_SYSCALL4(__xn_sys_trace, __xntrace_op_special_u64, id,
+				(unsigned long)(v >> 32),
+				(unsigned long)(v & 0xFFFFFFFF));
+}
+
