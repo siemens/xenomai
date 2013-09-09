siemens/xenomai

commit d8a7e68c6ab0adb4ae7bb913dd974ea1043a38b1

cobalt/kernel/posix: turn queues into regular kernel lists

kernel/cobalt/posix/registry.c
@@ -273,42 +273,32 @@ int cobalt_desc_get(cobalt_desc_t ** descp, int fd, unsigned magic)
 DEFINE_XNLOCK(cobalt_assoc_lock);
 
 static int cobalt_assoc_lookup_inner(cobalt_assocq_t * q,
-				    cobalt_assoc_t ** passoc,
-				    u_long key)
+				     cobalt_assoc_t ** passoc,
+				     unsigned long key)
 {
-	cobalt_assoc_t *assoc;
-	xnholder_t *holder;
+	cobalt_assoc_t *assoc = NULL;
 
-	holder = getheadq(q);
+	if (list_empty(q))
+		goto out;
 
-	if (!holder) {
-		/* empty list. */
-		*passoc = NULL;
-		return 0;
+	list_for_each_entry(assoc, q, link) {
+		if (assoc->key == key) {
+			*passoc = assoc;
+			return 1;
+		}
+		if (assoc->key > key)
+			goto out;
 	}
 
-	do {
-		assoc = link2assoc(holder);
-		holder = nextq(q, holder);
-	}
-	while (holder && (assoc->key < key));
-
-	if (assoc->key == key) {
-		/* found */
-		*passoc = assoc;
-		return 1;
-	}
-
-	/* not found. */
-	if (assoc->key < key)
-		*passoc = holder ? link2assoc(holder) : NULL;
-	else
-		*passoc = assoc;
+	passoc = NULL;
+out:
+	*passoc = assoc;
 
 	return 0;
 }
 
-int cobalt_assoc_insert(cobalt_assocq_t * q, cobalt_assoc_t * assoc, u_long key)
+int cobalt_assoc_insert(cobalt_assocq_t * q,
+			cobalt_assoc_t * assoc, unsigned long key)
 {
 	cobalt_assoc_t *next;
 	spl_t s;
@@ -321,18 +311,18 @@ int cobalt_assoc_insert(cobalt_assocq_t * q, cobalt_assoc_t * assoc, u_long key)
 	}
 
 	assoc->key = key;
-	inith(&assoc->link);
 	if (next)
-		insertq(q, &next->link, &assoc->link);
+		list_add_tail(&assoc->link, &next->link);
 	else
-		appendq(q, &assoc->link);
+		list_add_tail(&assoc->link, q);
 
 	xnlock_put_irqrestore(&cobalt_assoc_lock, s);
 
 	return 0;
 }
 
-cobalt_assoc_t *cobalt_assoc_lookup(cobalt_assocq_t * q, u_long key)
+cobalt_assoc_t *cobalt_assoc_lookup(cobalt_assocq_t * q,
+				    unsigned long key)
 {
 	cobalt_assoc_t *assoc;
 	unsigned found;
@@ -345,7 +335,8 @@ cobalt_assoc_t *cobalt_assoc_lookup(cobalt_assocq_t * q, u_long key)
 	return found ? assoc : NULL;
 }
 
-cobalt_assoc_t *cobalt_assoc_remove(cobalt_assocq_t * q, u_long key)
+cobalt_assoc_t *cobalt_assoc_remove(cobalt_assocq_t * q,
+				    unsigned long key)
 {
 	cobalt_assoc_t *assoc;
 	spl_t s;
@@ -356,7 +347,7 @@ cobalt_assoc_t *cobalt_assoc_remove(cobalt_assocq_t * q, u_long key)
 		return NULL;
 	}
 
-	removeq(q, &assoc->link);
+	list_del(&assoc->link);
 	xnlock_put_irqrestore(&cobalt_assoc_lock, s);
 
 	return assoc;
@@ -364,18 +355,22 @@ cobalt_assoc_t *cobalt_assoc_remove(cobalt_assocq_t * q, u_long key)
 
 void cobalt_assocq_destroy(cobalt_assocq_t * q, void (*destroy) (cobalt_assoc_t *))
 {
-	cobalt_assoc_t *assoc;
-	xnholder_t *holder;
+	cobalt_assoc_t *assoc, *tmp;
 	spl_t s;
 
 	xnlock_get_irqsave(&cobalt_assoc_lock, s);
-	while ((holder = getq(q))) {
-		assoc = link2assoc(holder);
+
+	if (list_empty(q))
+		goto out;
+
+	list_for_each_entry_safe(assoc, tmp, q, link) {
+		list_del(&assoc->link);
 		xnlock_put_irqrestore(&cobalt_assoc_lock, s);
 		if (destroy)
 			destroy(assoc);
 		xnlock_get_irqsave(&cobalt_assoc_lock, s);
 	}
+out:
 	xnlock_put_irqrestore(&cobalt_assoc_lock, s);
 }
 

kernel/cobalt/posix/cond.c
@@ -52,12 +52,12 @@
 #include "cond.h"
 
 static inline void
-cond_destroy_internal(cobalt_cond_t *cond, struct cobalt_kqueues *q)
+cond_destroy_internal(struct cobalt_cond *cond, struct cobalt_kqueues *q)
 {
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
-	removeq(&q->condq, &cond->link);
+	list_del(&cond->link);
 	/* synchbase wait queue may not be empty only when this function is
 	   called from cobalt_cond_pkg_cleanup, hence the absence of
 	   xnpod_schedule(). */
@@ -97,18 +97,17 @@ static inline int
 pthread_cond_init(struct __shadow_cond *cnd, const pthread_condattr_t *attr)
 {
 	xnflags_t synch_flags = XNSYNCH_PRIO | XNSYNCH_NOPIP;
+	struct list_head *condq, *entry;
 	struct xnsys_ppd *sys_ppd;
-	struct xnholder *holder;
-	cobalt_cond_t *cond;
-	xnqueue_t *condq;
+	struct cobalt_cond *cond;
 	spl_t s;
 	int err;
 
-	if (!attr)
+	if (attr == NULL)
 		attr = &cobalt_default_cond_attr;
 
-	cond = (cobalt_cond_t *)xnmalloc(sizeof(*cond));
-	if (!cond)
+	cond = xnmalloc(sizeof(*cond));
+	if (cond == NULL)
 		return -ENOMEM;
 
 	sys_ppd = xnsys_ppd_get(attr->pshared);
@@ -136,18 +135,19 @@ pthread_cond_init(struct __shadow_cond *cnd, const pthread_condattr_t *attr)
 	 * such condvar exits, we may assume that other processes
 	 * sharing that condvar won't be able to keep on running.
 	 */
-	if (cnd->magic == COBALT_COND_MAGIC) {
-		for (holder = getheadq(condq); holder;
-		     holder = nextq(condq, holder))
-			if (holder == &cnd->cond->link) {
-				if (attr->pshared) {
-					cond_destroy_internal(cnd->cond,
-							      cobalt_kqueues(1));
-					goto do_init;
-				}
-				err = -EBUSY;
-				goto err_free_pending_signals;
+	if (cnd->magic != COBALT_COND_MAGIC || list_empty(condq))
+		goto do_init;
+
+	list_for_each(entry, condq) {
+		if (entry == &cnd->cond->link) {
+			if (attr->pshared) {
+				cond_destroy_internal(cnd->cond,
+						      cobalt_kqueues(1));
+				goto do_init;
 			}
+			err = -EBUSY;
+			goto err_free_pending_signals;
+		}
 	}
 do_init:
 	cnd->attr = *attr;
@@ -161,12 +161,10 @@ do_init:
 
 	cond->magic = COBALT_COND_MAGIC;
 	xnsynch_init(&cond->synchbase, synch_flags, NULL);
-	inith(&cond->link);
 	cond->attr = *attr;
 	cond->mutex = NULL;
 	cond->owningq = cobalt_kqueues(attr->pshared);
-
-	appendq(condq, &cond->link);
+	list_add_tail(&cond->link, condq);
 
 	xnlock_put_irqrestore(&nklock, s);
 
@@ -205,7 +203,7 @@ do_init:
  */
 static inline int pthread_cond_destroy(struct __shadow_cond *cnd)
 {
-	cobalt_cond_t *cond;
+	struct cobalt_cond *cond;
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
@@ -238,8 +236,8 @@ static inline int pthread_cond_destroy(struct __shadow_cond *cnd)
 }
 
 static inline int cobalt_cond_timedwait_prologue(xnthread_t *cur,
-						 cobalt_cond_t *cond,
-						 cobalt_mutex_t *mutex,
+						 struct cobalt_cond *cond,
+						 struct cobalt_mutex *mutex,
 						 int timed,
 						 xnticks_t abs_to)
 {
@@ -279,8 +277,7 @@ static inline int cobalt_cond_timedwait_prologue(xnthread_t *cur,
 	/* Bind mutex to cond. */
 	if (cond->mutex == NULL) {
 		cond->mutex = mutex;
-		inith(&cond->mutex_link);
-		appendq(&mutex->conds, &cond->mutex_link);
+		list_add_tail(&cond->mutex_link, &mutex->conds);
 	}
 
 	/* Wait for another thread to signal the condition. */
@@ -316,8 +313,8 @@ static inline int cobalt_cond_timedwait_prologue(xnthread_t *cur,
 }
 
 static inline int cobalt_cond_timedwait_epilogue(xnthread_t *cur,
-						 cobalt_cond_t *cond,
-						 cobalt_mutex_t *mutex)
+						 struct cobalt_cond *cond,
+						 struct cobalt_mutex *mutex)
 {
 	int err;
 	spl_t s;
@@ -328,15 +325,16 @@ static inline int cobalt_cond_timedwait_epilogue(xnthread_t *cur,
 	if (err == -EINTR)
 		goto unlock_and_return;
 
-	/* Unbind mutex and cond, if no other thread is waiting, if the job was
-	   not already done. */
-	if (!xnsynch_pended_p(&cond->synchbase)
-	    && cond->mutex == mutex) {
+	/*
+	 * Unbind mutex and cond, if no other thread is waiting, if
+	 * the job was not already done.
+	 */
+	if (!xnsynch_pended_p(&cond->synchbase) && cond->mutex == mutex) {
 		cond->mutex = NULL;
-		removeq(&mutex->conds, &cond->mutex_link);
+		list_del(&cond->mutex_link);
 	}
 
-      unlock_and_return:
+unlock_and_return:
 	xnlock_put_irqrestore(&nklock, s);
 
 	return err;
@@ -396,10 +394,10 @@ int cobalt_cond_wait_prologue(struct __shadow_cond __user *u_cnd,
 			      struct timespec __user *u_ts)
 {
 	xnthread_t *cur = xnshadow_current();
+	struct cobalt_cond *cnd;
+	struct cobalt_mutex *mx;
 	struct mutex_dat *datp;
 	struct us_cond_data d;
-	cobalt_cond_t *cnd;
-	cobalt_mutex_t *mx;
 	struct timespec ts;
 	int err, perr = 0;
 
@@ -455,8 +453,8 @@ int cobalt_cond_wait_epilogue(struct __shadow_cond __user *u_cnd,
 			      struct __shadow_mutex __user *u_mx)
 {
 	xnthread_t *cur = xnshadow_current();
-	cobalt_cond_t *cnd;
-	cobalt_mutex_t *mx;
+	struct cobalt_cond *cnd;
+	struct cobalt_mutex *mx;
 	int err;
 
 	__xn_get_user(cnd, &u_cnd->cond);
@@ -474,27 +472,29 @@ int cobalt_cond_wait_epilogue(struct __shadow_cond __user *u_cnd,
 
 void cobalt_condq_cleanup(struct cobalt_kqueues *q)
 {
-	xnholder_t *holder;
+	struct cobalt_cond *cond, *tmp;
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
 
-	while ((holder = getheadq(&q->condq)) != NULL) {
+	if (list_empty(&q->condq))
+		goto out;
+
+	list_for_each_entry_safe(cond, tmp, &q->condq, link) {
 		xnlock_put_irqrestore(&nklock, s);
-		cond_destroy_internal(link2cond(holder), q);
+		cond_destroy_internal(cond, q);
 #if XENO_DEBUG(COBALT)
-		printk(XENO_INFO "deleting Cobalt condvar %p\n",
-			 link2cond(holder));
+		printk(XENO_INFO "deleting Cobalt condvar %p\n", cond);
 #endif /* XENO_DEBUG(COBALT) */
 		xnlock_get_irqsave(&nklock, s);
 	}
-
+out:
 	xnlock_put_irqrestore(&nklock, s);
 }
 
 void cobalt_cond_pkg_init(void)
 {
-	initq(&cobalt_global_kqueues.condq);
+	INIT_LIST_HEAD(&cobalt_global_kqueues.condq);
 }
 
 void cobalt_cond_pkg_cleanup(void)

kernel/cobalt/posix/cond.h
@@ -50,22 +50,17 @@ union cobalt_cond_union {
 struct __shadow_mutex;
 union cobalt_mutex_union;
 
-typedef struct cobalt_cond {
-	unsigned magic;
-	xnsynch_t synchbase;
-	xnholder_t link;	/* Link in cobalt_condq */
-
-#define link2cond(laddr) container_of(laddr, cobalt_cond_t, link)
-
-	xnholder_t mutex_link;
-
-#define mutex_link2cond(laddr) container_of(laddr, cobalt_cond_t, mutex_link)
-
+struct cobalt_cond {
+	unsigned int magic;
+	struct xnsynch synchbase;
+	/** cobalt_condq */
+	struct list_head link;
+	struct list_head mutex_link;
 	unsigned long *pending_signals;
 	pthread_condattr_t attr;
 	struct cobalt_mutex *mutex;
 	struct cobalt_kqueues *owningq;
-} cobalt_cond_t;
+};
 
 extern const pthread_condattr_t cobalt_default_cond_attr;
 

kernel/cobalt/posix/event.c
@@ -82,12 +82,11 @@ int cobalt_event_init(struct cobalt_event_shadow __user *u_evtsh,
 	synflags = (flags & COBALT_EVENT_PRIO) ? XNSYNCH_PRIO : XNSYNCH_FIFO;
 	xnsynch_init(&event->synch, synflags, NULL);
 	event->magic = COBALT_EVENT_MAGIC;
-	inith(&event->link);
 	kq = cobalt_kqueues(pshared);
 	event->owningq = kq;
 
 	xnlock_get_irqsave(&nklock, s);
-	appendq(&kq->eventq, &event->link);
+	list_add_tail(&event->link, &kq->eventq);
 	xnlock_put_irqrestore(&nklock, s);
 
 	datp->value = value;
@@ -245,7 +244,7 @@ static void cobalt_event_destroy_inner(struct cobalt_event *event,
 	struct xnheap *heap;
 	int pshared;
 
-	removeq(&q->eventq, &event->link);
+	list_del(&event->link);
 	xnsynch_destroy(&event->synch);
 	event->magic = 0;
 	pshared = (event->flags & COBALT_EVENT_SHARED) != 0;
@@ -284,23 +283,23 @@ out:
 
 void cobalt_eventq_cleanup(struct cobalt_kqueues *q)
 {
-	struct cobalt_event *event;
-	struct xnholder *h;
+	struct cobalt_event *event, *tmp;
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
 
-	while ((h = getheadq(&q->eventq))) {
-		event = container_of(h, struct cobalt_event, link);
-		cobalt_event_destroy_inner(event, q, s);
-	}
+	if (list_empty(&q->eventq))
+		goto out;
 
+	list_for_each_entry_safe(event, tmp, &q->eventq, link)
+		cobalt_event_destroy_inner(event, q, s);
+out:
 	xnlock_put_irqrestore(&nklock, s);
 }
 
 void cobalt_event_pkg_init(void)
 {
-	initq(&cobalt_global_kqueues.eventq);
+	INIT_LIST_HEAD(&cobalt_global_kqueues.eventq);
 }
 
 void cobalt_event_pkg_cleanup(void)

kernel/cobalt/posix/event.h
@@ -39,7 +39,7 @@ struct cobalt_event {
 	struct xnsynch synch;
 	struct cobalt_event_data *data;
 	struct cobalt_kqueues *owningq;
-	struct xnholder link;
+	struct list_head link;
 	int flags;
 };
 

kernel/cobalt/posix/init.c
@@ -92,7 +92,7 @@ int __init cobalt_init(void)
 	cobalt_monitor_pkg_init();
 	cobalt_timer_pkg_init();
 
-	initq(&cobalt_global_kqueues.threadq);
+	INIT_LIST_HEAD(&cobalt_global_kqueues.threadq);
 	cobalt_time_slice = CONFIG_XENO_OPT_RR_QUANTUM * 1000;
 
 	return 0;

kernel/cobalt/posix/internal.h
@@ -35,6 +35,7 @@
 #include <cobalt/kernel/ppd.h>
 #include <cobalt/kernel/select.h>
 #include <cobalt/kernel/assert.h>
+#include <cobalt/kernel/list.h>
 #include <cobalt/syscall.h>
 #include <asm/xenomai/arith.h>
 #include "registry.h"
@@ -69,14 +70,13 @@
 #define cobalt_mark_deleted(t) ((t)->magic = ~(t)->magic)
 
 struct cobalt_kqueues {
-	struct xnqueue condq;
-	struct xnqueue intrq;
-	struct xnqueue mutexq;
-	struct xnqueue semq;
-	struct xnqueue threadq;
-	struct xnqueue timerq;
-	struct xnqueue monitorq;
-	struct xnqueue eventq;
+	struct list_head condq;
+	struct list_head mutexq;
+	struct list_head semq;
+	struct list_head threadq;
+	struct list_head timerq;
+	struct list_head monitorq;
+	struct list_head eventq;
 };
 
 struct cobalt_context {

kernel/cobalt/posix/monitor.c
@@ -86,13 +86,12 @@ int cobalt_monitor_init(struct cobalt_monitor_shadow __user *u_monsh,
 	xnsynch_init(&mon->drain, XNSYNCH_PRIO, NULL);
 	mon->flags = flags;
 	mon->magic = COBALT_MONITOR_MAGIC;
-	inith(&mon->link);
-	initq(&mon->waiters);
+	INIT_LIST_HEAD(&mon->waiters);
 	kq = cobalt_kqueues(pshared);
 	mon->owningq = kq;
 
 	xnlock_get_irqsave(&nklock, s);
-	appendq(&kq->monitorq, &mon->link);
+	list_add_tail(&mon->link, &kq->monitorq);
 	xnlock_put_irqrestore(&nklock, s);
 
 	datp->flags = 0;
@@ -163,8 +162,7 @@ static void cobalt_monitor_wakeup(struct cobalt_monitor *mon)
 {
 	struct cobalt_monitor_data *datp = mon->data;
 	struct xnthread *p;
-	struct xnholder *h;
-	pthread_t tid;
+	pthread_t tid, tmp;
 	int bcast;
 
 	/*
@@ -174,7 +172,7 @@ static void cobalt_monitor_wakeup(struct cobalt_monitor *mon)
 	 */
 	bcast = (datp->flags & COBALT_MONITOR_BROADCAST) != 0;
 	if ((datp->flags & COBALT_MONITOR_GRANTED) == 0 ||
-	    emptyq_p(&mon->waiters))
+	    list_empty(&mon->waiters))
 		goto drain;
 
 	/*
@@ -187,10 +185,7 @@ static void cobalt_monitor_wakeup(struct cobalt_monitor *mon)
 	 * syscall for exiting the monitor if nobody else is waiting
 	 * at the gate.
 	 */
-	h = getheadq(&mon->waiters);
-	while (h) {
-		tid = container_of(h, struct cobalt_thread, monitor_link);
-		h = nextq(&mon->waiters, h);
+	list_for_each_entry_safe(tid, tmp, &mon->waiters, monitor_link) {
 		p = &tid->threadbase;
 		/*
 		 * A thread might receive a grant signal albeit it
@@ -201,7 +196,7 @@ static void cobalt_monitor_wakeup(struct cobalt_monitor *mon)
 		if (bcast ||
 		    (p->u_window->grant_value && p->wchan == &tid->monitor_synch)) {
 			xnsynch_wakeup_this_sleeper(&tid->monitor_synch, p);
-			removeq(&mon->waiters, &tid->monitor_link);
+			list_del(&tid->monitor_link);
 			tid->monitor_queued = 0;
 		}
 	}
@@ -219,8 +214,7 @@ drain:
 			xnsynch_wakeup_one_sleeper(&mon->drain);
 	}
 
-	if (emptyq_p(&mon->waiters) &&
-	    !xnsynch_pended_p(&mon->drain))
+	if (list_empty(&mon->waiters) && !xnsynch_pended_p(&mon->drain))
 		datp->flags &= ~COBALT_MONITOR_PENDED;
 }
 
@@ -273,7 +267,7 @@ int cobalt_monitor_wait(struct cobalt_monitor_shadow __user *u_monsh,
 		synch = &mon->drain;
 	else {
 		cur->threadbase.u_window->grant_value = 0;
-		appendq(&mon->waiters, &cur->monitor_link);
+		list_add_tail(&cur->monitor_link, &mon->waiters);
 		cur->monitor_queued = 1;
 	}
 	datp->flags |= COBALT_MONITOR_PENDED;
@@ -289,11 +283,11 @@ int cobalt_monitor_wait(struct cobalt_monitor_shadow __user *u_monsh,
 
 		if ((event & COBALT_MONITOR_WAITDRAIN) == 0 &&
 		    cur->monitor_queued) {
-			removeq(&mon->waiters, &cur->monitor_link);
+			list_del(&cur->monitor_link);
 			cur->monitor_queued = 0;
 		}
 
-		if (emptyq_p(&mon->waiters) && !xnsynch_pended_p(&mon->drain))
+		if (list_empty(&mon->waiters) && !xnsynch_pended_p(&mon->drain))
 			datp->flags &= ~COBALT_MONITOR_PENDED;
 
 		if (info & XNBREAK)
@@ -374,7 +368,7 @@ static void cobalt_monitor_destroy_inner(struct cobalt_monitor *mon,
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
-	removeq(&q->monitorq, &mon->link);
+	list_del(&mon->link);
 	xnsynch_destroy(&mon->gate);
 	xnsynch_destroy(&mon->drain);
 	mon->magic = 0;
@@ -403,7 +397,7 @@ int cobalt_monitor_destroy(struct cobalt_monitor_shadow __user *u_monsh)
 		goto fail;
 	}
 
-	if (xnsynch_pended_p(&mon->drain) || !emptyq_p(&mon->waiters)) {
+	if (xnsynch_pended_p(&mon->drain) || !list_empty(&mon->waiters)) {
 		ret = -EBUSY;
 		goto fail;
 	}
@@ -432,25 +426,26 @@ int cobalt_monitor_destroy(struct cobalt_monitor_shadow __user *u_monsh)
 
 void cobalt_monitorq_cleanup(struct cobalt_kqueues *q)
 {
-	struct cobalt_monitor *mon;
-	struct xnholder *h;
+	struct cobalt_monitor *mon, *tmp;
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
 
-	while ((h = getheadq(&q->monitorq))) {
+	if (list_empty(&q->monitorq))
+		goto out;
+
+	list_for_each_entry_safe(mon, tmp, &q->monitorq, link) {
 		xnlock_put_irqrestore(&nklock, s);
-		mon = container_of(h, struct cobalt_monitor, link);
 		cobalt_monitor_destroy_inner(mon, q);
 		xnlock_get_irqsave(&nklock, s);
 	}
-
+out:
 	xnlock_put_irqrestore(&nklock, s);
 }
 
 void cobalt_monitor_pkg_init(void)
 {
-	initq(&cobalt_global_kqueues.monitorq);
+	INIT_LIST_HEAD(&cobalt_global_kqueues.monitorq);
 }
 
 void cobalt_monitor_pkg_cleanup(void)

commit 18241591a9e5860ec2bff0be4e9d94545ae528ff

cobalt/posix/registry: fix regression introduced by d8a7e68

kernel/cobalt/posix/registry.c
@@ -291,7 +291,7 @@ static int cobalt_assoc_lookup_inner(struct list_head *q,
 			goto out;
 	}
 
-	passoc = NULL;
+	assoc = NULL;
 out:
 	*passoc = assoc;
 