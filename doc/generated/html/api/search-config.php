f02fd6cf29a83610fb02ba71cb66b7cc00277e3f
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2013-10-13 19:14:13 +0200

    doc: regenerate

--- doc/generated/html/api/search-config.php
+++ doc/generated/html/api/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "<div id=\"side-nav\" class=\"ui-resizable side-nav-resizable\">\n  <div id=\"nav-tree\">\n    <div id=\"nav-tree-contents\">\n      <div id=\"nav-sync\" class=\"sync\"></div>\n    </div>\n  </div>\n  <div id=\"splitbar\" style=\"-moz-user-select:none;\" \n       class=\"ui-resizable-handle\">\n  </div>\n</div>\n<script type=\"text/javascript\">\n$(document).ready(function(){initNavTree('.html','');});\n</script>\n<div id=\"doc-content\">\n",
-  'logo' => "Generated on Fri Oct 4 2013 22:39:09 for Xenomai API by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.1.2 ",
+  'logo' => "Generated on Sun Oct 13 2013 19:13:11 for Xenomai API by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.1.2 ",
 );
 
 </script>
