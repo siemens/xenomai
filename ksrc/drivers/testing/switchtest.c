db9bdd711ffbc02953ec424311c97705861b18a3
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2013-12-03 21:19:03 +0100

    licenses: jumbo update
    Rules followed are:
    - kernel-space .c -> GPL
    - user/kernel .h -> GPL
    - non testsuite user-space .h -> LGPL
    - non testsuite user-space .c -> LGPL
    - user-space testsuite -> MIT

--- ksrc/drivers/testing/switchtest.c
+++ ksrc/drivers/testing/switchtest.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2006-2013 Gilles Chanteperdrix <gch@xenomai.org>.
+ *
+ * Xenomai is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Xenomai is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Xenomai; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include <nucleus/synch.h>
 #include <nucleus/thread.h>
 #include <nucleus/trace.h>
@@ -42,7 +60,7 @@ module_param(start_index, uint, 0400);
 MODULE_PARM_DESC(start_index, "First device instance number to be used");
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Gilles.Chanteperdrix@laposte.net");
+MODULE_AUTHOR("Gilles.Chanteperdrix@Xenomai.org");
 
 static void handle_ktask_error(rtswitch_context_t *ctx, unsigned fp_val)
 {
