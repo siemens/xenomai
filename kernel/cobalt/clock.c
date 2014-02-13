ddd1c81cbd0595496fe02864974a5763fd348779
Philippe Gerum <rpm@xenomai.org>
2014-02-13 10:47:30 +0100

    cobalt/clock: enable clocks over non-rt CPUs as well
    We may want to read time from non-RT CPUs.

--- kernel/cobalt/clock.c
+++ kernel/cobalt/clock.c
@@ -253,7 +253,7 @@ static void adjust_clock_timers(struct xnclock *clock, xnsticks_t delta)
 	INIT_LIST_HEAD(&adjq);
 	delta = xnclock_ns_to_ticks(clock, delta);
 
-	for_each_realtime_cpu(cpu) {
+	for_each_online_cpu(cpu) {
 		sched = xnsched_struct(cpu);
 		q = &xnclock_percpu_timerdata(clock, cpu)->q;
 
@@ -489,7 +489,7 @@ int xnclock_register(struct xnclock *clock)
 	if (clock->timerdata == NULL)
 		return -ENOMEM;
 
-	for_each_realtime_cpu(cpu) {
+	for_each_online_cpu(cpu) {
 		tmd = xnclock_percpu_timerdata(clock, cpu);
 		xntimerq_init(&tmd->q);
 	}
@@ -529,7 +529,7 @@ void xnclock_deregister(struct xnclock *clock)
 
 	cleanup_clock_proc(clock);
 
-	for_each_realtime_cpu(cpu) {
+	for_each_online_cpu(cpu) {
 		tmd = xnclock_percpu_timerdata(clock, cpu);
 		XENO_BUGON(NUCLEUS, !xntimerq_empty(&tmd->q));
 		xntimerq_destroy(&tmd->q);
