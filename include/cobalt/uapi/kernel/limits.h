1ce3308baaf1bec858b26809456750ab23d4415d
Philippe Gerum <rpm@xenomai.org>
2014-04-16 10:45:21 +0200

    include/cobalt,mercury: introduce core-specific limits.h

--- include/cobalt/uapi/kernel/limits.h
+++ include/cobalt/uapi/kernel/limits.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Philippe Gerum <rpm@xenomai.org>.
+ * Copyright (C) 2014 Philippe Gerum <rpm@xenomai.org>.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -15,27 +15,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
-#ifndef _COBALT_UAPI_KERNEL_TYPES_H
-#define _COBALT_UAPI_KERNEL_TYPES_H
+#ifndef _COBALT_UAPI_KERNEL_LIMITS_H
+#define _COBALT_UAPI_KERNEL_LIMITS_H
 
-#include <cobalt/uapi/kernel/limits.h>
+#define XNOBJECT_NAME_LEN 32
 
-typedef unsigned long long xnticks_t;
-
-typedef long long xnsticks_t;
-
-typedef unsigned long long xntime_t; /* ns */
-
-typedef long long xnstime_t;
-
-typedef unsigned long xnhandle_t;
-
-#define XN_NO_HANDLE ((xnhandle_t)0)
-
-#define XN_HANDLE_SPARE0	((xnhandle_t)0x10000000)
-#define XN_HANDLE_SPARE1	((xnhandle_t)0x20000000)
-#define XN_HANDLE_SPARE2	((xnhandle_t)0x40000000)
-#define XN_HANDLE_SPARE3	((xnhandle_t)0x80000000)
-#define XN_HANDLE_SPARE_MASK	((xnhandle_t)0xf0000000)
-
-#endif /* !_COBALT_UAPI_KERNEL_TYPES_H */
+#endif /* !_COBALT_UAPI_KERNEL_LIMITS_H */
