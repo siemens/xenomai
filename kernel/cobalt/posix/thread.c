siemens/xenomai

commit f15920c67d075cee304e673ad2b76610dda3b97d

cobalt/sched: assoc policy identifier to sched class

kernel/cobalt/posix/thread.c
@@ -264,7 +264,6 @@ pthread_setschedparam_ex(struct cobalt_thread *thread,
 		ret = -EINVAL;
 		goto out;
 	}
-	thread->sched_u_policy = policy;
 	xnthread_set_slice(&thread->threadbase, tslice);
 	if (cobalt_call_extension(thread_setsched, &thread->extref, ret,
 				  sched_class, &param) && ret)
@@ -284,7 +283,6 @@ pthread_getschedparam_ex(struct cobalt_thread *thread,
 {
 	struct xnsched_class *base_class;
 	struct xnthread *base_thread;
-	int prio;
 	spl_t s;
 
 	xnlock_get_irqsave(&nklock, s);
@@ -297,9 +295,10 @@ pthread_getschedparam_ex(struct cobalt_thread *thread,
 
 	base_thread = &thread->threadbase;
 	base_class = base_thread->base_class;
-	*policy_r = thread->sched_u_policy;
-	prio = xnthread_base_priority(base_thread);
-	param_ex->sched_priority = prio;
+	param_ex->sched_priority = xnthread_base_priority(base_thread);
+	*policy_r = base_class->policy;
+	if (param_ex->sched_priority == 0) /* SCHED_FIFO/SCHED_WEAK */
+		*policy_r = SCHED_NORMAL;
 
 	if (base_class == &xnsched_class_rt) {
 		if (xnthread_test_state(base_thread, XNRRB))
@@ -379,7 +378,6 @@ static inline int pthread_create(struct cobalt_thread **thread_p,
 		return ret;
 	}
 
-	thread->sched_u_policy = policy;
 	thread->magic = COBALT_THREAD_MAGIC;
 	xnsynch_init(&thread->monitor_synch, XNSYNCH_FIFO, NULL);
 

include/cobalt/kernel/sched.h
@@ -158,6 +158,7 @@ struct xnsched_class {
 	int nthreads;
 	struct xnsched_class *next;
 	int weight;
+	int policy;
 	const char *name;
 };
 

kernel/cobalt/posix/thread.h
@@ -102,9 +102,6 @@ struct cobalt_thread {
 	struct xnsynch sigwait;
 	struct list_head signext;
 
-	/** Cached value for current policy (user side). */
-	int sched_u_policy;
-
 	/** Monitor wait object and link holder. */
 	struct xnsynch monitor_synch;
 	struct list_head monitor_link;

kernel/cobalt/sched-idle.c
@@ -56,5 +56,6 @@ struct xnsched_class xnsched_class_idle = {
 	.sched_getparam		=	xnsched_idle_getparam,
 	.sched_trackprio	=	xnsched_idle_trackprio,
 	.weight			=	XNSCHED_CLASS_WEIGHT(0),
+	.policy			=	SCHED_IDLE,
 	.name			=	"idle"
 };

kernel/cobalt/sched-quota.c
@@ -19,6 +19,7 @@
 #include <linux/bitmap.h>
 #include <cobalt/kernel/sched.h>
 #include <cobalt/kernel/arith.h>
+#include <cobalt/uapi/sched.h>
 
 /*
  * With this policy, each per-CPU scheduler slot maintains a list of
@@ -765,6 +766,7 @@ struct xnsched_class xnsched_class_quota = {
 	.sched_cleanup_vfile	=	xnsched_quota_cleanup_vfile,
 #endif
 	.weight			=	XNSCHED_CLASS_WEIGHT(3),
+	.policy			=	SCHED_QUOTA,
 	.name			=	"quota"
 };
 EXPORT_SYMBOL_GPL(xnsched_class_quota);

kernel/cobalt/sched-rt.c
@@ -246,6 +246,7 @@ struct xnsched_class xnsched_class_rt = {
 	.sched_cleanup_vfile	=	xnsched_rt_cleanup_vfile,
 #endif
 	.weight			=	XNSCHED_CLASS_WEIGHT(4),
+	.policy			=	SCHED_FIFO,
 	.name			=	"rt"
 };
 EXPORT_SYMBOL_GPL(xnsched_class_rt);

kernel/cobalt/sched-sporadic.c
@@ -18,6 +18,7 @@
  */
 #include <cobalt/kernel/sched.h>
 #include <cobalt/kernel/heap.h>
+#include <cobalt/uapi/sched.h>
 
 #define MAX_REPLENISH CONFIG_XENO_OPT_SCHED_SPORADIC_MAXREPL
 
@@ -537,6 +538,7 @@ struct xnsched_class xnsched_class_sporadic = {
 	.sched_cleanup_vfile	=	xnsched_sporadic_cleanup_vfile,
 #endif
 	.weight			=	XNSCHED_CLASS_WEIGHT(3),
+	.policy			=	SCHED_SPORADIC,
 	.name			=	"pss"
 };
 EXPORT_SYMBOL_GPL(xnsched_class_sporadic);

kernel/cobalt/sched-tp.c
@@ -18,6 +18,7 @@
  */
 #include <cobalt/kernel/sched.h>
 #include <cobalt/kernel/heap.h>
+#include <cobalt/uapi/sched.h>
 
 static void tp_schedule_next(struct xnsched_tp *tp)
 {
@@ -431,6 +432,7 @@ struct xnsched_class xnsched_class_tp = {
 	.sched_cleanup_vfile	=	xnsched_tp_cleanup_vfile,
 #endif
 	.weight			=	XNSCHED_CLASS_WEIGHT(2),
+	.policy			=	SCHED_TP,
 	.name			=	"tp"
 };
 EXPORT_SYMBOL_GPL(xnsched_class_tp);

commit 9974f1359394888ad3a3b75cdbd67234fbe8fb93

cobalt/posix/thread: fix pthread_getschedparam() for RR threads
f15920c67 introduced a regression, causing round-robin threads to
receive SCHED_FIFO instead of SCHED_RR upon inquiry for schedparams.

kernel/cobalt/posix/thread.c
@@ -301,8 +301,10 @@ pthread_getschedparam_ex(struct cobalt_thread *thread,
 		*policy_r = SCHED_NORMAL;
 
 	if (base_class == &xnsched_class_rt) {
-		if (xnthread_test_state(base_thread, XNRRB))
+		if (xnthread_test_state(base_thread, XNRRB)) {
 			ns2ts(&param_ex->sched_rr_quantum, base_thread->rrperiod);
+			*policy_r = SCHED_RR;
+		}
 		goto unlock_and_exit;
 	}
 