c794c9249a5f343975a8f072ea1b18fc1a3f20c6
Philippe Gerum <rpm@xenomai.org>
2015-01-15 11:44:49 +0100

    hal/arm: add specific calibration value for imx6q

--- include/asm-arm/calibration.h
+++ include/asm-arm/calibration.h
@@ -41,6 +41,8 @@ static inline unsigned long xnarch_get_sched_latency (void)
 	return 8500;
 #elif defined(CONFIG_ARCH_AT91SAM9263)
 	return 11000;
+#elif defined(CONFIG_SOC_IMX6Q)
+	return 6000;
 #elif defined(CONFIG_ARCH_MX51)
 	return 5000;
 #elif defined(CONFIG_ARCH_MX53)
