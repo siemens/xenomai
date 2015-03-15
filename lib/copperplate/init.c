siemens/xenomai

commit ede23e4c81749c394868025e9fe7b2f3f4d8db00

copperplate/registry: Establish per-user namespaces for named sessions
This moves non-anonymous sessions into user-specific namespaces:
/registry-root/user. This helps to isolate users from each other. The
layout for the shared anon session remains unmodified.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

lib/copperplate/init.c
@@ -46,7 +46,7 @@ struct coppernode __node_info = {
 	.reset_session = 0,
 	.silent_mode = 0,
 #ifdef CONFIG_XENO_REGISTRY
-	.session_label = DEFAULT_REGISTRY_SESSION,
+	.session_label = NULL,
 	.registry_root = DEFAULT_REGISTRY_ROOT,
 #endif
 };

lib/copperplate/registry.c
@@ -35,6 +35,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <fuse.h>
+#include <pwd.h>
 #include <xeno_config.h>
 #include "boilerplate/hash.h"
 #include "copperplate/heapobj.h"
@@ -787,11 +788,19 @@ int __registry_pkg_init(const char *arg0, char *mountpt, int shared_registry)
 
 int registry_pkg_init(const char *arg0)
 {
+	struct passwd *pw = NULL;
 	char *mountpt, *sessdir;
 	int ret;
 
-	ret = asprintf(&sessdir, "%s/%s",
-		       __node_info.registry_root, __node_info.session_label);
+	if (__node_info.session_label) {
+		pw = getpwuid(geteuid());
+		if (!pw)
+			return -errno;
+		ret = asprintf(&sessdir, "%s/%s/%s", __node_info.registry_root,
+			       pw->pw_name, __node_info.session_label);
+	} else
+		ret = asprintf(&sessdir, "%s/%s", __node_info.registry_root,
+			       DEFAULT_REGISTRY_SESSION);
 	if (ret < 0)
 		return -ENOMEM;
 

lib/copperplate/regd/regd.c
@@ -34,6 +34,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pwd.h>
 #include <boilerplate/list.h>
 #include <boilerplate/hash.h>
 #include "../internal.h"
@@ -50,7 +51,7 @@
 			printf("regd: " fmt "\n", ##args);	\
 	} while (0)
 
-static const char *rootdir = DEFAULT_REGISTRY_ROOT"/"DEFAULT_REGISTRY_SESSION;
+static char *rootdir;
 
 static int sockfd;
 
@@ -406,6 +407,7 @@ static void create_system_fs(const char *arg0, const char *rootdir)
 
 int main(int argc, char *const *argv)
 {
+	struct passwd *pw = NULL;
 	int lindex, opt, ret;
 	struct sigaction sa;
 
@@ -429,6 +431,18 @@ int main(int argc, char *const *argv)
 			usage();
 			exit(1);
 		}
+	}
+
+	if (rootdir == NULL) {
+		pw = getpwuid(geteuid());
+		if (!pw)
+			return -errno;
+		ret = asprintf(&rootdir, "%s/%s/%s",
+			       DEFAULT_REGISTRY_ROOT,
+			       pw->pw_name,
+			       DEFAULT_REGISTRY_SESSION);
+		if (ret < 0)
+			return -ENOMEM;
 	}
 
 	memset(&sa, 0, sizeof(sa));

commit 8c60d68e2296cf71f1b43ca6f0bc91788b1af78c

copperplate/init: fix shared heap name with default session
We need a valid session label to be defined before creating the shared
heap.  This fixes a regression introduced by ede23e4.

lib/copperplate/init.c
@@ -26,6 +26,7 @@
 #include <memory.h>
 #include <malloc.h>
 #include <assert.h>
+#include <pwd.h>
 #include <signal.h>
 #include <errno.h>
 #include <getopt.h>
@@ -45,10 +46,9 @@ struct coppernode __node_info = {
 	.no_sanity = !CONFIG_XENO_SANITY,
 	.reset_session = 0,
 	.silent_mode = 0,
-#ifdef CONFIG_XENO_REGISTRY
-	.session_label = NULL,
 	.registry_root = DEFAULT_REGISTRY_ROOT,
-#endif
+	.session_label = NULL,
+	.session_root = NULL,
 };
 
 pid_t __node_id;
@@ -517,6 +517,29 @@ fail:
 	early_panic("initialization failed, %s", symerror(ret));
 }
 
+static int get_session_root(void)
+{
+	struct passwd *pw;
+	char *sessdir;
+	int ret;
+
+	if (__node_info.session_label) {
+		pw = getpwuid(geteuid());
+		if (pw == NULL)
+			return -errno;
+		ret = asprintf(&sessdir, "%s/%s/%s", __node_info.registry_root,
+			       pw->pw_name, __node_info.session_label);
+	} else {
+		__node_info.session_label = DEFAULT_REGISTRY_SESSION;
+		ret = asprintf(&sessdir, "%s/%s", __node_info.registry_root,
+			       DEFAULT_REGISTRY_SESSION);
+	}
+
+	__node_info.session_root = sessdir;
+
+	return ret < 0 ? -ENOMEM : 0;
+}
+
 /* The application-level copperplate init call. */
 
 void copperplate_init(int *argcp, char *const **argvp)
@@ -586,6 +609,14 @@ void copperplate_init(int *argcp, char *const **argvp)
 		warning("failed to initialize main private heap");
 		goto fail;
 	}
+
+	/*
+	 * We need the session label to be known before we create the
+	 * shared heap, which is named after the former.
+	 */
+	ret = get_session_root();
+	if (ret)
+		goto fail;
 
 	ret = heapobj_pkg_init_shared();
 	if (ret) {

lib/copperplate/internal.h
@@ -32,13 +32,16 @@
 
 #ifdef CONFIG_XENO_REGISTRY
 #define DEFAULT_REGISTRY_ROOT		CONFIG_XENO_REGISTRY_ROOT
-#define DEFAULT_REGISTRY_SESSION	"anon"
+#else
+#define DEFAULT_REGISTRY_ROOT		NULL
 #endif
+#define DEFAULT_REGISTRY_SESSION	"anon"
 
 struct coppernode {
 	unsigned int mem_pool;
-	const char *session_label;
 	const char *registry_root;
+	const char *session_label;
+	const char *session_root;
 	cpu_set_t cpu_affinity;
 	int no_mlock;
 	int no_registry;

lib/copperplate/registry.c
@@ -35,7 +35,6 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <fuse.h>
-#include <pwd.h>
 #include <xeno_config.h>
 #include "boilerplate/hash.h"
 #include "copperplate/heapobj.h"
@@ -802,24 +801,10 @@ int __registry_pkg_init(const char *arg0, char *mountpt, int shared_registry)
 
 int registry_pkg_init(const char *arg0)
 {
-	struct passwd *pw = NULL;
-	char *mountpt, *sessdir;
+	char *mountpt;
 	int ret;
 
-	if (__node_info.session_label) {
-		pw = getpwuid(geteuid());
-		if (!pw)
-			return -errno;
-		ret = asprintf(&sessdir, "%s/%s/%s", __node_info.registry_root,
-			       pw->pw_name, __node_info.session_label);
-	} else
-		ret = asprintf(&sessdir, "%s/%s", __node_info.registry_root,
-			       DEFAULT_REGISTRY_SESSION);
-	if (ret < 0)
-		return -ENOMEM;
-
-	ret = connect_regd(sessdir, &mountpt);
-	free(sessdir);
+	ret = connect_regd(__node_info.session_root, &mountpt);
 	if (ret)
 		return ret;
 