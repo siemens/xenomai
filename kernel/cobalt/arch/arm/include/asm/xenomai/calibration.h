2c1ee1232c67dc33173083540d006f5478b1766c
Philippe Gerum <rpm@xenomai.org>
2015-07-03 10:00:17 +0200

    cobalt/arm: add calibration value for LS1021A

--- kernel/cobalt/arch/arm/include/asm/xenomai/calibration.h
+++ kernel/cobalt/arch/arm/include/asm/xenomai/calibration.h
@@ -44,6 +44,8 @@ static inline void xnarch_get_latencies(struct xnclock_gravity *p)
 	ulat = 2000;
 #elif defined(CONFIG_SOC_IMX7)
 	ulat = 2000;
+#elif defined(CONFIG_SOC_LS1021A)
+	ulat = 2800;
 #elif defined(CONFIG_ARCH_OMAP)
 	ulat = cpu_is_omap44xx() ? 2500 : 5000;
 #else
