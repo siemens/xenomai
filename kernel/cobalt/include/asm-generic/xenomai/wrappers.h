1178e9c639624168e98811ce5d5a9e9f9e4e302c
Philippe Gerum <rpm@xenomai.org>
2015-11-03 13:11:25 +0100

    cobalt/kernel: fixup for v3.19+ (trace_seq)

--- kernel/cobalt/include/asm-generic/xenomai/wrappers.h
+++ kernel/cobalt/include/asm-generic/xenomai/wrappers.h
@@ -67,6 +67,7 @@
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0)
 #include <linux/netdevice.h>
+#include <linux/trace_seq.h>
 
 #undef alloc_netdev
 #define alloc_netdev(sizeof_priv, name, name_assign_type, setup) \
@@ -79,7 +80,6 @@ trace_seq_buffer_ptr(struct trace_seq *s)
 {
 	return s->buffer + s->len;
 }
-
 #endif /* < 3.17 */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,16,0)
