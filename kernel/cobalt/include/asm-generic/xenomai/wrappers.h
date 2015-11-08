bb57c06eee0c52956e58fc0fba2bd29c9484a0fe
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2015-11-08 13:22:35 +0100

    cobalt/wrappers: add wrappers for igb driver

--- kernel/cobalt/include/asm-generic/xenomai/wrappers.h
+++ kernel/cobalt/include/asm-generic/xenomai/wrappers.h
@@ -39,26 +39,75 @@
  *   symbol, so that obsolete wrappers can be spotted.
  */
 
+#define XENO_BACKPORT(__sym) xeno_backport_ ##__sym
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,11,0)
 #define DEVICE_ATTR_RW(_name)	__ATTR_RW(_name)
 #define DEVICE_ATTR_RO(_name)	__ATTR_RO(_name)
 #define DEVICE_ATTR_WO(_name)	__ATTR_WO(_name)
 #endif /* < 3.11 */
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0)
+#include <linux/dma-mapping.h>
+#include <linux/hwmon.h>
+
+#define dma_set_mask_and_coherent \
+	XENO_BACKPORT(dma_set_mask_and_coherent)
+static inline int dma_set_mask_and_coherent(struct device *dev, u64 mask)
+{
+	int rc = dma_set_mask(dev, mask);
+	if (rc == 0)
+		dma_set_coherent_mask(dev, mask);
+	return rc;
+}
+
+#ifdef CONFIG_HWMON
+#define hwmon_device_register_with_groups \
+	XENO_BACKPORT(hwmon_device_register_with_groups)
+struct device *
+hwmon_device_register_with_groups(struct device *dev, const char *name,
+				void *drvdata,
+				const struct attribute_group **groups);
+
+#define devm_hwmon_device_register_with_groups \
+	XENO_BACKPORT(devm_hwmon_device_register_with_groups)
+struct device *
+devm_hwmon_device_register_with_groups(struct device *dev, const char *name,
+				void *drvdata,
+				const struct attribute_group **groups);
+#endif /* hwmon */
+#endif /* < 3,13 */
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,14,0)
+#include <linux/pci.h>
+
 #define get_current_uuid() current_uid()
+
+#ifdef CONFIG_PCI
+#define pci_enable_msix_range XENO_BACKPORT(pci_enable_msix_range)
+#ifdef CONFIG_PCI_MSI
+int pci_enable_msix_range(struct pci_dev *dev,
+			struct msix_entry *entries,
+			int minvec, int maxvec);
+#else /* not pci msi */
+static inline int pci_enable_msix_range(struct pci_dev *dev,
+					struct msix_entry *entries,
+					int minvec, int maxvec)
+{ return -ENOSYS; }
+#endif /* not pci msi */
+#endif /* pci */
 #else /* >= 3.14 */
 #define get_current_uuid() from_kuid_munged(current_user_ns(), current_uid())
-#endif
+#endif /* >= 3.14 */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,15,0)
 #define raw_cpu_ptr(v)	__this_cpu_ptr(v)
-#endif
+#endif /* < 3.15 */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,16,0)
 #define smp_mb__before_atomic()  smp_mb()
 #define smp_mb__after_atomic()   smp_mb()
-#endif
+#endif /* < 3.16 */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0)
 #include <linux/netdevice.h>
@@ -66,6 +115,6 @@
 #undef alloc_netdev
 #define alloc_netdev(sizeof_priv, name, name_assign_type, setup) \
 	alloc_netdev_mqs(sizeof_priv, name, setup, 1, 1)
-#endif
+#endif /* < 3.17 */
 
 #endif /* _COBALT_ASM_GENERIC_WRAPPERS_H */
