72d9188d032b90ede05c47d197f864c7a36ade84
Philippe Gerum <rpm@xenomai.org>
2016-03-03 09:16:21 +0100

    cobalt/wrappers: add wrapper for reinit_completion()

--- kernel/cobalt/include/asm-generic/xenomai/wrappers.h
+++ kernel/cobalt/include/asm-generic/xenomai/wrappers.h
@@ -67,7 +67,6 @@
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0)
 #include <linux/netdevice.h>
-#include <linux/trace_seq.h>
 
 #undef alloc_netdev
 #define alloc_netdev(sizeof_priv, name, name_assign_type, setup) \
@@ -80,6 +79,7 @@ trace_seq_buffer_ptr(struct trace_seq *s)
 {
 	return s->buffer + s->len;
 }
+
 #endif /* < 3.17 */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,16,0)
