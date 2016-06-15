siemens/xenomai

commit b51bb27b3e73e68b9d63a00b11596229ebb31a67

Introduce host tick deferral

include/nucleus/timer.h
@@ -410,6 +410,8 @@ static inline void xntimer_set_name(xntimer_t *timer, const char *name)
 #endif /* CONFIG_XENO_OPT_STATS */
 }
 
+void xntimer_next_local_shot(struct xnsched *sched);
+
 /*!
  * \addtogroup timer
  *@{ */

ksrc/nucleus/timer.c
@@ -55,18 +55,57 @@ static inline void xntimer_dequeue_aperiodic(xntimer_t *timer)
 	__setbits(timer->status, XNTIMER_DEQUEUED);
 }
 
-static void xntimer_next_local_shot(xnsched_t *this_sched)
+void xntimer_next_local_shot(xnsched_t *sched)
 {
-	xntimerh_t *holder = xntimerq_head(&this_sched->timerqueue);
+	struct xntimer *timer;
 	xnsticks_t delay;
-	xntimer_t *timer;
+	xntimerq_it_t it;
+	xntimerh_t *h;
 
-	/* Do not reprogram locally when inside the tick handler - will be
-	   done on exit anyway. Also exit if there is no pending timer. */
-	if (testbits(this_sched->status, XNINTCK) || !holder)
+	/*
+	 * Do not reprogram locally when inside the tick handler -
+	 * will be done on exit anyway. Also exit if there is no
+	 * pending timer.
+	 */
+	if (testbits(sched->status, XNINTCK))
 		return;
 
-	timer = aplink2timer(holder);
+	h = xntimerq_it_begin(&sched->timerqueue, &it);
+	if (h == NULL)
+		return;
+
+	/*
+	 * Here we try to defer the host tick heading the timer queue,
+	 * so that it does not preempt a real-time activity uselessly,
+	 * in two cases:
+	 *
+	 * 1) a rescheduling is pending for the current CPU. We may
+	 * assume that a real-time thread is about to resume, so we
+	 * want to move the host tick out of the way until the host
+	 * kernel resumes, unless there is no other outstanding
+	 * timers.
+	 *
+	 * 2) the current thread is running in primary mode, in which
+	 * case we may also defer the host tick until the host kernel
+	 * resumes.
+	 *
+	 * The host tick deferral is cleared whenever Xenomai is about
+	 * to yield control to the host kernel (see
+	 * __xnpod_schedule()), or a timer with an earlier timeout
+	 * date is scheduled, whichever comes first.
+	 */
+	__clrbits(sched->status, XNHDEFER);
+	timer = aplink2timer(h);
+	if (unlikely(timer == &sched->htimer)) {
+		if (xnsched_self_resched_p(sched) ||
+		    !xnthread_test_state(sched->curr, XNROOT)) {
+			h = xntimerq_it_next(&sched->timerqueue, &it, h);
+			if (h) {
+				__setbits(sched->status, XNHDEFER);
+				timer = aplink2timer(h);
+			}
+		}
+	}
 
 	delay = xntimerh_date(&timer->aplink) -
 		(xnarch_get_cpu_tsc() + nklatency);
@@ -81,9 +120,23 @@ static void xntimer_next_local_shot(xnsched_t *this_sched)
 	xnarch_program_timer_shot(delay);
 }
 
-static inline int xntimer_heading_p(xntimer_t *timer)
+static inline int xntimer_heading_p(struct xntimer *timer)
 {
-	return xntimerq_head(&timer->sched->timerqueue) == &timer->aplink;
+	struct xnsched *sched = timer->sched;
+	xntimerq_it_t it;
+	xntimerh_t *h;
+
+	h = xntimerq_it_begin(&sched->timerqueue, &it);
+	if (h == &timer->aplink)
+		return 1;
+
+	if (testbits(sched->status, XNHDEFER)) {
+		h = xntimerq_it_next(&sched->timerqueue, &it, h);
+		if (h == &timer->aplink)
+			return 1;
+	}
+
+	return 0;
 }
 
 static inline void xntimer_next_remote_shot(xnsched_t *sched)
@@ -339,11 +392,15 @@ void xntimer_tick_aperiodic(void)
 				continue;
 			}
 		} else {
-			/* By postponing the propagation of the low-priority host
-			   tick to the interrupt epilogue (see
-			   xnintr_irq_handler()), we save some I-cache, which
-			   translates into precious microsecs on low-end hw. */
+			/*
+			 * By postponing the propagation of the
+			 * low-priority host tick to the interrupt
+			 * epilogue (see xnintr_irq_handler()), we
+			 * save some I-cache, which translates into
+			 * precious microsecs on low-end hw.
+			 */
 			__setbits(sched->status, XNHTICK);
+			__clrbits(sched->status, XNHDEFER);
 			if (!testbits(timer->status, XNTIMER_PERIODIC))
 				continue;
 		}

include/nucleus/sched.h
@@ -48,6 +48,7 @@
 #define XNINIRQ		0x08000000	/* In IRQ handling context */
 #define XNSWLOCK	0x04000000	/* In context switch */
 #define XNRESCHED	0x02000000	/* Needs rescheduling */
+#define XNHDEFER	0x01000000	/* Host tick deferred */
 
 struct xnsched_rt {
 	xnsched_queue_t runnable;	/*!< Runnable thread queue. */
@@ -160,9 +161,14 @@ struct xnsched_class {
 #endif /* CONFIG_SMP */
 
 /* Test all resched flags from the given scheduler mask. */
-static inline int xnsched_resched_p(xnsched_t *sched) 
+static inline int xnsched_resched_p(struct xnsched *sched)
 {
 	return !xnarch_cpus_empty(sched->resched);
+}
+
+static inline int xnsched_self_resched_p(struct xnsched *sched)
+{
+	return xnarch_cpu_isset(xnsched_cpu(sched), sched->resched);
 }
 
 /* Set self resched flag for the given scheduler. */

ksrc/nucleus/pod.c
@@ -2166,8 +2166,11 @@ void __xnpod_schedule(struct xnsched *sched)
 
 	if (xnthread_test_state(prev, XNROOT))
 		xnarch_leave_root(xnthread_archtcb(prev));
-	else if (xnthread_test_state(next, XNROOT))
+	else if (xnthread_test_state(next, XNROOT)) {
+		if (testbits(sched->status, XNHDEFER))
+			xntimer_next_local_shot(sched);
 		xnarch_enter_root(xnthread_archtcb(next));
+	}
 
 	xnstat_exectime_switch(sched, &next->stat.account);
 	xnstat_counter_inc(&next->stat.csw);

commit 8e1e2e37f9ee510f19417ea4ad873fdd560918fe

nucleus/timers: fix the timers situation
Remove bheap and timerwheel timer list structures, as they had been
broken by commit b51bb27b3e73e68b9d63a00b11596229ebb31a67. Introduce
rbtree, and use it instead for large number of timers, make it the
default on x86_64 so that it gets tested.

include/nucleus/Makefile.am
@@ -2,7 +2,6 @@ includesubdir = $(includedir)/nucleus
 
 includesub_HEADERS = \
 	assert.h \
-	bheap.h \
 	bufd.h \
 	compiler.h \
 	heap.h \

include/nucleus/bheap.h
@@ -1,242 +1,0 @@
-/*
- * Copyright (C) 2006 Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
- *
- * Xenomai is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- *
- * Xenomai is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Xenomai; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
- */
-
-#ifndef _XENO_NUCLEUS_BHEAP_H
-#define _XENO_NUCLEUS_BHEAP_H
-
-#include <nucleus/compiler.h>
-
-/* debug support */
-#include <nucleus/assert.h>
-
-/* Priority queue implementation, using a binary heap. */
-
-typedef unsigned long long bheap_key_t;
-
-typedef struct bheaph {
-	bheap_key_t key;
-	unsigned prio;
-	unsigned pos;
-} bheaph_t;
-
-#define bheaph_init(holder) do { } while (0)
-#define bheaph_key(holder)  ((holder)->key)
-#define bheaph_prio(holder) ((holder)->prio)
-#define bheaph_pos(holder)  ((holder)->pos)
-#define bheaph_lt(h1, h2)   ((long long) ((h1)->key - (h2)->key) < 0 ||	\
-			     ((h1)->key == (h2)->key &&			\
-			      (h1)->prio > (h2)->prio))
-
-typedef struct bheap {
-	unsigned sz;
-	unsigned last;
-	bheaph_t *elems[];
-} bheap_t;
-
-#define DECLARE_BHEAP_CONTAINER(name, sz)       \
-	struct {				\
-		bheap_t bheap;			\
-		bheaph_t *elems[sz + 1];	\
-	} name
-
-/* Check the binary heap invariant. */
-static inline int bheap_ordered(bheap_t *heap)
-{
-	unsigned i;
-	for (i = 2; i < heap->last; i++)
-		if (bheaph_lt(heap->elems[i], heap->elems[i / 2]))
-			return 0;
-	return 1;
-}
-
-#define BHEAP_CHECK(heap)						\
-	XENO_BUGON(QUEUES, ((heap)->sz == 0) || !bheap_ordered(heap))
-
-#define bheap_gethead(heap)				\
-	({                                              \
-		bheap_t *_bheap = &(heap)->bheap;	\
-		BHEAP_CHECK(_bheap);			\
-		__internal_bheap_gethead(_bheap);	\
-	})
-
-static inline bheaph_t *__internal_bheap_gethead(bheap_t *heap)
-{
-	if (heap->last == 1)
-		return NULL;
-
-	return heap->elems[1];
-}
-
-#define bheap_next(heap, holder)			\
-	({						\
-		bheap_t *_bheap = &(heap)->bheap;	\
-		BHEAP_CHECK(_bheap);			\
-		__internal_bheap_next(_bheap, holder);	\
-	})
-
-static inline bheaph_t *__internal_bheap_next(bheap_t *heap, bheaph_t *holder)
-{
-	unsigned pos;
-
-	if (unlikely(bheaph_pos(holder) >= heap->last
-		     || heap->elems[bheaph_pos(holder)] != holder))
-		return (bheaph_t *) ERR_PTR(-EINVAL);
-
-	pos = bheaph_pos(holder) + 1;
-
-	return likely(pos < heap->last) ? heap->elems[pos] : NULL;
-}
-
-static inline bheaph_t *bheaph_parent(bheap_t *heap, bheaph_t *holder)
-{
-	const unsigned pos = holder->pos;
-
-	return likely(pos > 1) ? heap->elems[pos / 2] : NULL;
-}
-
-static inline bheaph_t *bheaph_child(bheap_t *heap, bheaph_t *holder, int side)
-{
-	const unsigned pos = 2 * holder->pos + side;
-
-	return likely(pos < heap->last) ? heap->elems[pos] : NULL;
-}
-
-#define bheap_init(heap, sz) __internal_bheap_init(&(heap)->bheap, sz)
-
-static inline void __internal_bheap_init(bheap_t *heap, unsigned sz)
-{
-	heap->sz = sz;
-	heap->last = 1;
-}
-
-#define bheap_destroy(heap) __internal_bheap_destroy(&(heap)->bheap)
-
-static inline void __internal_bheap_destroy(bheap_t *heap)
-{
-	heap->sz = 0;
-	heap->last = 1;
-}
-
-static inline void bheap_swap(bheap_t *heap, bheaph_t *h1, bheaph_t *h2)
-{
-	const unsigned pos2 = bheaph_pos(h2);
-
-	heap->elems[bheaph_pos(h1)] = h2;
-	bheaph_pos(h2) = bheaph_pos(h1);
-	heap->elems[pos2] = h1;
-	bheaph_pos(h1) = pos2;
-}
-
-static inline void bheap_up(bheap_t *heap, bheaph_t *holder)
-{
-	bheaph_t *parent;
-
-	while ((parent = bheaph_parent(heap, holder)) && bheaph_lt(holder, parent))
-		bheap_swap(heap, holder, parent);
-}
-
-static inline void bheap_down(bheap_t *heap, bheaph_t *holder)
-{
-	bheaph_t *left, *right, *minchild;
-
-	for (;;) {
-		left = bheaph_child(heap, holder, 0);
-		right = bheaph_child(heap, holder, 1);
-
-		if (left && right)
-			minchild = bheaph_lt(left, right) ? left : right;
-		else
-			minchild = left ?: right;
-
-		if (!minchild || bheaph_lt(holder, minchild))
-			break;
-
-		bheap_swap(heap, minchild, holder);
-	}
-}
-
-#define bheap_insert(heap, holder)				\
-	({							\
-		bheap_t *_bheap = &(heap)->bheap;		\
-		BHEAP_CHECK(_bheap);				\
-		__internal_bheap_insert(_bheap, holder);	\
-	})
-
-static inline int __internal_bheap_insert(bheap_t *heap, bheaph_t *holder)
-{
-	if (heap->last == heap->sz + 1)
-		return EBUSY;
-
-	heap->elems[heap->last] = holder;
-	bheaph_pos(holder) = heap->last;
-	++heap->last;
-	bheap_up(heap, holder);
-	return 0;
-}
-
-#define bheap_delete(heap, holder)				\
-	({							\
-		bheap_t *_bheap = &(heap)->bheap;		\
-		BHEAP_CHECK(_bheap);				\
-		__internal_bheap_delete(_bheap, holder);	\
-	})
-
-static inline int __internal_bheap_delete(bheap_t *heap, bheaph_t *holder)
-{
-	bheaph_t *lasth;
-
-	if (unlikely(bheaph_pos(holder) >= heap->last
-		     || heap->elems[bheaph_pos(holder)] != holder))
-		return EINVAL;
-
-	--heap->last;
-	if (heap->last != bheaph_pos(holder)) {
-		bheaph_t *parent;
-		lasth = heap->elems[heap->last];
-		heap->elems[bheaph_pos(holder)] = lasth;
-		bheaph_pos(lasth) = bheaph_pos(holder);
-		if ((parent = bheaph_parent(heap, lasth)) && bheaph_lt(lasth, parent))
-			bheap_up(heap, lasth);
-		else
-			bheap_down(heap, lasth);
-	}
-
-	return 0;
-}
-
-#define bheap_get(heap)					\
-	({                                              \
-		bheap_t *_bheap = &(heap)->bheap;	\
-		BHEAP_CHECK(_bheap);			\
-		__internal_bheap_get(_bheap, holder);	\
-	})
-
-static inline bheaph_t *__internal_bheap_get(bheap_t *heap)
-{
-	bheaph_t *holder = __internal_bheap_gethead(heap);
-
-	if (!holder)
-		return NULL;
-
-	__internal_bheap_delete(heap, holder);
-
-	return holder;
-}
-
-#endif /* _XENO_NUCLEUS_BHEAP_H */

include/nucleus/timer.h
@@ -83,6 +83,8 @@ typedef struct {
 		!_h ? NULL : link2tlholder(_h);		\
 	})
 
+#define xntlist_second(q, h) xntlist_next((q),(h))
+
 static inline void xntlist_insert(xnqueue_t *q, xntlholder_t *holder)
 {
 	xnholder_t *p;
@@ -103,147 +105,59 @@ static inline void xntlist_insert(xnqueue_t *q, xntlholder_t *holder)
 
 #define xntlist_remove(q, h)  removeq((q),&(h)->link)
 
-#if defined(CONFIG_XENO_OPT_TIMER_HEAP)
+#if defined(CONFIG_XENO_OPT_TIMER_RBTREE)
 
-#include <nucleus/bheap.h>
+#include <linux/rbtree.h>
 
-typedef bheaph_t xntimerh_t;
+typedef struct {
+	unsigned long long date;
+	unsigned prio;
+	struct rb_node link;
+} xntimerh_t;
 
-#define xntimerh_date(h)          bheaph_key(h)
-#define xntimerh_prio(h)          bheaph_prio(h)
-#define xntimerh_init(h)          bheaph_init(h)
+#define xntimerh_date(h) ((h)->date)
+#define xntimerh_prio(h) ((h)->prio)
+#define xntimerh_init(h) do { } while (0)
 
-typedef DECLARE_BHEAP_CONTAINER(xntimerq_t, CONFIG_XENO_OPT_TIMER_HEAP_CAPACITY);
-
-#define xntimerq_init(q)          bheap_init((q), CONFIG_XENO_OPT_TIMER_HEAP_CAPACITY)
-#define xntimerq_destroy(q)       bheap_destroy(q)
-#define xntimerq_head(q)          bheap_gethead(q)
-#define xntimerq_insert(q, h)     bheap_insert((q),(h))
-#define xntimerq_remove(q, h)     bheap_delete((q),(h))
-
-typedef struct {} xntimerq_it_t;
-
-#define xntimerq_it_begin(q, i)   ((void) (i), bheap_gethead(q))
-#define xntimerq_it_next(q, i, h) ((void) (i), bheap_next((q),(h)))
-
-#elif defined(CONFIG_XENO_OPT_TIMER_WHEEL)
-
-typedef xntlholder_t xntimerh_t;
-
-#define xntimerh_date(h)       xntlholder_date(h)
-#define xntimerh_prio(h)       xntlholder_prio(h)
-#define xntimerh_init(h)       xntlholder_init(h)
-
-typedef struct xntimerq {
-	unsigned date_shift;
-	unsigned long long next_shot;
-	unsigned long long shot_wrap;
-	xnqueue_t bucket[XNTIMER_WHEELSIZE];
+typedef struct {
+	struct rb_root root;
+	xntimerh_t *head;
 } xntimerq_t;
 
-typedef struct xntimerq_it {
-	unsigned bucket;
-} xntimerq_it_t;
+#define xntimerq_init(q)			\
+	({					\
+		xntimerq_t *_q = (q);		\
+		_q->root = RB_ROOT;		\
+		_q->head = NULL;		\
+	})
 
-static inline void xntimerq_init(xntimerq_t *q)
+#define xntimerq_destroy(q) do { } while (0)
+#define xntimerq_empty(q) ((q)->head != NULL)
+
+#define xntimerq_head(q) ((q)->head)
+
+#define xntimerq_next(q, h)						\
+	({								\
+		struct rb_node *_node = rb_next(&(h)->link);		\
+		_node ? (container_of(_node, xntimerh_t, link)) : NULL; \
+	})
+
+#define xntimerq_second(q, h) xntimerq_next(q, h)
+
+void xntimerq_insert(xntimerq_t *q, xntimerh_t *holder);
+
+static inline void xntimerq_remove(xntimerq_t *q, xntimerh_t *holder)
 {
-	unsigned long long step_tsc;
-	unsigned i;
+	if (holder == q->head)
+		q->head = xntimerq_second(q, holder);
 
-	step_tsc = xnarch_ns_to_tsc(CONFIG_XENO_OPT_TIMER_WHEEL_STEP);
-	/* q->date_shift = fls(step_tsc); */
-	for (q->date_shift = 0; (1 << q->date_shift) < step_tsc; q->date_shift++)
-		;
-	q->next_shot = q->shot_wrap = ((~0ULL) >> q->date_shift) + 1;
-	for (i = 0; i < sizeof(q->bucket)/sizeof(xnqueue_t); i++)
-		xntlist_init(&q->bucket[i]);
+	rb_erase(&holder->link, &q->root);
 }
 
-#define xntimerq_destroy(q)    do { } while (0)
+typedef struct { } xntimerq_it_t;
 
-static inline xntlholder_t *xntimerq_head(xntimerq_t *q)
-{
-	unsigned bucket = ((unsigned) q->next_shot) & XNTIMER_WHEELMASK;
-	xntlholder_t *result;
-	unsigned i;
-
-	if (q->next_shot == q->shot_wrap)
-		return NULL;
-
-	result = xntlist_head(&q->bucket[bucket]);
-
-	if (result && (xntlholder_date(result) >> q->date_shift) == q->next_shot)
-		return result;
-
-	/* We could not find the next timer in the first bucket, iterate over
-	   the other buckets. */
-	for (i = (bucket + 1) & XNTIMER_WHEELMASK ;
-	     i != bucket; i = (i + 1) & XNTIMER_WHEELMASK) {
-		xntlholder_t *candidate = xntlist_head(&q->bucket[i]);
-
-		if(++q->next_shot == q->shot_wrap)
-			q->next_shot = 0;
-
-		if (!candidate)
-			continue;
-
-		if ((xntlholder_date(candidate) >> q->date_shift) == q->next_shot)
-			return candidate;
-
-		if (!result || (xnsticks_t) (xntlholder_date(candidate)
-					     - xntlholder_date(result)) < 0)
-			result = candidate;
-	}
-
-	if (result)
-		q->next_shot = (xntlholder_date(result) >> q->date_shift);
-	else
-		q->next_shot = q->shot_wrap;
-	return result;
-}
-
-static inline void xntimerq_insert(xntimerq_t *q, xntimerh_t *h)
-{
-	unsigned long long shifted_date = xntlholder_date(h) >> q->date_shift;
-	unsigned bucket = ((unsigned) shifted_date) & XNTIMER_WHEELMASK;
-
-	if ((long long) (shifted_date - q->next_shot) < 0)
-		q->next_shot = shifted_date;
-	xntlist_insert(&q->bucket[bucket], h);
-}
-
-static inline void xntimerq_remove(xntimerq_t *q, xntimerh_t *h)
-{
-	unsigned long long shifted_date = xntlholder_date(h) >> q->date_shift;
-	unsigned bucket = ((unsigned) shifted_date) & XNTIMER_WHEELMASK;
-
-	xntlist_remove(&q->bucket[bucket], h);
-	/* Do not attempt to update q->next_shot, xntimerq_head will recover. */
-}
-
-static inline xntimerh_t *xntimerq_it_begin(xntimerq_t *q, xntimerq_it_t *it)
-{
-	xntimerh_t *holder = NULL;
-
-	for (it->bucket = 0; it->bucket < XNTIMER_WHEELSIZE; it->bucket++)
-		if ((holder = xntlist_head(&q->bucket[it->bucket])))
-			break;
-
-	return holder;
-}
-
-static inline xntimerh_t *
-xntimerq_it_next(xntimerq_t *q, xntimerq_it_t *it, xntimerh_t *holder)
-{
-	xntimerh_t *next = xntlist_next(&q->bucket[it->bucket], holder);
-
-	if (!next)
-		for(it->bucket++; it->bucket < XNTIMER_WHEELSIZE; it->bucket++)
-			if ((next = xntlist_head(&q->bucket[it->bucket])))
-				break;
-
-	return next;
-}
+#define xntimerq_it_begin(q,i)	((void) (i), xntimerq_head(q))
+#define xntimerq_it_next(q,i,h) ((void) (i), xntimerq_next((q),(h)))
 
 #else /* CONFIG_XENO_OPT_TIMER_LIST */
 
@@ -258,6 +172,7 @@ typedef xnqueue_t xntimerq_t;
 #define xntimerq_init(q)        xntlist_init(q)
 #define xntimerq_destroy(q)     do { } while (0)
 #define xntimerq_head(q)        xntlist_head(q)
+#define xntimerq_second(q,h)	xntlist_second((q),(h))
 #define xntimerq_insert(q,h)    xntlist_insert((q),(h))
 #define xntimerq_remove(q, h)   xntlist_remove((q),(h))
 

ksrc/nucleus/Kconfig
@@ -447,7 +447,8 @@ config XENO_OPT_SCALABLE_SCHED
 
 choice
 	prompt "Timer indexing method"
-	default XENO_OPT_TIMER_LIST
+	default XENO_OPT_TIMER_LIST if !X86_64
+	default XENO_OPT_TIMER_RBTREE if X86_64
 	help
 
 	This option allows to select the underlying data structure
@@ -464,43 +465,15 @@ config XENO_OPT_TIMER_LIST
 	particularly efficient when only a few timers (< 10) may be
 	concurrently outstanding at any point in time.
 
-config XENO_OPT_TIMER_HEAP
+config XENO_OPT_TIMER_RBTREE
 	bool "Tree"
 	help
 
-	Use a binary heap. This data structure is efficient when a
+	Use a red-black tree. This data structure is efficient when a
 	high number of software timers may be concurrently
 	outstanding at any point in time.
 
-config XENO_OPT_TIMER_WHEEL
-	bool "Hash"
-	help
-
-	Use a hash table. Timers operations using this data structure
-	should have an O(1) complexity if the timers follow two
-	conditions:
-	- timers expiration dates do not collide too much;
-	- there is at least one periodic timer using a period near
-	the wheel step (around 100000 ns by default).
-
 endchoice
-
-config XENO_OPT_TIMER_HEAP_CAPACITY
-	int "Binary heap capacity"
-	depends on XENO_OPT_TIMER_HEAP
-	default 256
-	help
-
-	Set the maximum number of timers in the nucleus timers list.
-
-config XENO_OPT_TIMER_WHEEL_STEP
-	int "Timer wheel step"
-	depends on XENO_OPT_TIMER_WHEEL
-	default 100000
-	help
-
-	Set the duration in ns of a timer wheel step. At each step,
-	the timer wheel use the next hash bucket.
 
 endmenu
 

ksrc/nucleus/timer.c
@@ -59,7 +59,6 @@ void xntimer_next_local_shot(xnsched_t *sched)
 {
 	struct xntimer *timer;
 	xnsticks_t delay;
-	xntimerq_it_t it;
 	xntimerh_t *h;
 
 	/*
@@ -70,7 +69,7 @@ void xntimer_next_local_shot(xnsched_t *sched)
 	if (testbits(sched->status, XNINTCK))
 		return;
 
-	h = xntimerq_it_begin(&sched->timerqueue, &it);
+	h = xntimerq_head(&sched->timerqueue);
 	if (h == NULL)
 		return;
 
@@ -99,7 +98,7 @@ void xntimer_next_local_shot(xnsched_t *sched)
 	if (unlikely(timer == &sched->htimer)) {
 		if (xnsched_resched_p(sched) ||
 		    !xnthread_test_state(sched->curr, XNROOT)) {
-			h = xntimerq_it_next(&sched->timerqueue, &it, h);
+			h = xntimerq_second(&sched->timerqueue, h);
 			if (h) {
 				__setbits(sched->lflags, XNHDEFER);
 				timer = aplink2timer(h);
@@ -123,15 +122,14 @@ void xntimer_next_local_shot(xnsched_t *sched)
 static inline int xntimer_heading_p(struct xntimer *timer)
 {
 	struct xnsched *sched = timer->sched;
-	xntimerq_it_t it;
 	xntimerh_t *h;
 
-	h = xntimerq_it_begin(&sched->timerqueue, &it);
+	h = xntimerq_head(&sched->timerqueue);
 	if (h == &timer->aplink)
 		return 1;
 
 	if (testbits(sched->lflags, XNHDEFER)) {
-		h = xntimerq_it_next(&sched->timerqueue, &it, h);
+		h = xntimerq_second(&sched->timerqueue, h);
 		if (h == &timer->aplink)
 			return 1;
 	}
@@ -1165,4 +1163,35 @@ void xntimer_cleanup_proc(void)
 
 #endif /* CONFIG_XENO_OPT_VFILE */
 
+#if defined(CONFIG_XENO_OPT_TIMER_RBTREE)
+static inline bool xntimerh_is_lt(xntimerh_t *left, xntimerh_t *right)
+{
+	return left->date < right->date
+		|| (left->date == right->date && left->prio > right->prio);
+}
+
+void xntimerq_insert(xntimerq_t *q, xntimerh_t *holder)
+{
+	struct rb_node **new = &q->root.rb_node, *parent = NULL;
+
+	if (!q->head)
+		q->head = holder;
+	else if (xntimerh_is_lt(holder, q->head)) {
+		parent = &q->head->link;
+		new = &parent->rb_left;
+		q->head = holder;
+	} else while (*new) {
+		xntimerh_t *i = container_of(*new, xntimerh_t, link);
+
+		parent = *new;
+		if (xntimerh_is_lt(holder, i))
+			new = &((*new)->rb_left);
+		else
+			new = &((*new)->rb_right);
+	}
+
+	rb_link_node(&holder->link, parent, new);
+	rb_insert_color(&holder->link, &q->root);
+}
+#endif
 /*@}*/