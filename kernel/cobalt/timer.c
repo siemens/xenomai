siemens/xenomai

commit ccbd3750c1af2c2e65e2d04e038f6a7bf194e5ca

cobalt/timer: guard against invalid calls to get_overruns()
Calculating the number of overruns only makes sense with active
periodic timers exclusively, which must have been requeued for the
next delivery already.

kernel/cobalt/timer.c
@@ -593,6 +593,9 @@ unsigned long long xntimer_get_overruns(struct xntimer *timer, xnticks_t now)
 	xnsticks_t delta;
 	unsigned long long overruns = 0;
 
+	XENO_BUG_ON(COBALT, (timer->status &
+	     (XNTIMER_DEQUEUED|XNTIMER_PERIODIC)) != XNTIMER_PERIODIC);
+	
 	delta = now - xntimer_pexpect(timer);
 	if (unlikely(delta >= (xnsticks_t) period)) {
 		xntimerq_t *q;
@@ -604,8 +607,7 @@ unsigned long long xntimer_get_overruns(struct xntimer *timer, xnticks_t now)
 
 		if (xntimer_running_p(timer)) {
 			q = xntimer_percpu_queue(timer);
-			if ((timer->status & XNTIMER_DEQUEUED) == 0)
-				xntimer_dequeue(timer, q);
+			xntimer_dequeue(timer, q);
 			while (xntimerh_date(&timer->aplink) < now) {
 				timer->periodic_ticks++;
 				xntimer_update_date(timer);

commit 0326d526edeb377948b68d86a51b25a403b67c81

cobalt/timer: fix spurious assertion trigger
Commit #ccbd3750c introduced a debug assertion which only makes sense
when the timer is running. Reduce the scope of this check in order to
eliminate triggers on false positive.

kernel/cobalt/timer.c
@@ -601,23 +601,21 @@ void xntimer_release_ipi(void)
 unsigned long long xntimer_get_overruns(struct xntimer *timer, xnticks_t now)
 {
 	xnticks_t period = timer->interval;
-	xnsticks_t delta;
 	unsigned long long overruns = 0;
+	xnsticks_t delta;
+	xntimerq_t *q;
 
-	XENO_BUG_ON(COBALT, (timer->status &
-	     (XNTIMER_DEQUEUED|XNTIMER_PERIODIC)) != XNTIMER_PERIODIC);
-	
 	delta = now - xntimer_pexpect(timer);
 	if (unlikely(delta >= (xnsticks_t) period)) {
-		xntimerq_t *q;
-
 		period = timer->interval_ns;
 		delta = xnclock_ticks_to_ns(xntimer_clock(timer), delta);
 		overruns = xnarch_div64(delta, period);
 		timer->pexpect_ticks += overruns;
-
 		if (xntimer_running_p(timer)) {
-			q = xntimer_percpu_queue(timer);
+			XENO_BUG_ON(COBALT, (timer->status &
+				    (XNTIMER_DEQUEUED|XNTIMER_PERIODIC))
+				    != XNTIMER_PERIODIC);
+				q = xntimer_percpu_queue(timer);
 			xntimer_dequeue(timer, q);
 			while (xntimerh_date(&timer->aplink) < now) {
 				timer->periodic_ticks++;
@@ -628,6 +626,7 @@ unsigned long long xntimer_get_overruns(struct xntimer *timer, xnticks_t now)
 	}
 
 	timer->pexpect_ticks++;
+
 	return overruns;
 }
 EXPORT_SYMBOL_GPL(xntimer_get_overruns);