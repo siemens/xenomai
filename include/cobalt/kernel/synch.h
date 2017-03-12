9000dc953d740fa4bf48db88d9c7c0b4de69407b
Philippe Gerum <rpm@xenomai.org>
2017-03-12 10:23:47 +0100

    cobalt/synch: add __must_check annotation to lock acquisition routines

--- include/cobalt/kernel/synch.h
+++ include/cobalt/kernel/synch.h
@@ -149,11 +149,11 @@ int xnsynch_wakeup_many_sleepers(struct xnsynch *synch, int nr);
 void xnsynch_wakeup_this_sleeper(struct xnsynch *synch,
 				 struct xnthread *sleeper);
 
-int xnsynch_acquire(struct xnsynch *synch,
-		    xnticks_t timeout,
-		    xntmode_t timeout_mode);
+int __must_check xnsynch_acquire(struct xnsynch *synch,
+				 xnticks_t timeout,
+				 xntmode_t timeout_mode);
 
-int xnsynch_try_acquire(struct xnsynch *synch);
+int __must_check xnsynch_try_acquire(struct xnsynch *synch);
 
 struct xnthread *xnsynch_release(struct xnsynch *synch,
 				 struct xnthread *thread);
