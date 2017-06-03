881461f1381f68201c1ceb8ad46e904b9e43528e
Philippe Gerum <rpm@xenomai.org>
2017-06-03 17:48:50 +0200

    cobalt/arm: add calibration data for STIH4x SoC

--- kernel/cobalt/arch/arm/include/asm/xenomai/calibration.h
+++ kernel/cobalt/arch/arm/include/asm/xenomai/calibration.h
@@ -48,6 +48,8 @@ static inline void xnarch_get_latencies(struct xnclock_gravity *p)
 	ulat = 2800;
 #elif defined(CONFIG_ARCH_OMAP)
 	ulat = cpu_is_omap44xx() ? 2500 : 5000;
+#elif defined(CONFIG_ARCH_STI)
+	ulat = 6000;
 #else
 	ulat = 9500;	/* XXX sane? */
 #endif
